811a382727917041361dee83a6eb25eee9ebcd99
Michael Loring <mdloring@gmail.com>
2016-02-20 03:12:20 -0600

    load individual section front headline slider

--- parts/headlines/headlines-slider.php
+++ parts/headlines/headlines-slider.php
@@ -39,6 +39,21 @@
 			case 'news':
 				dynamic_sidebar( 'news_headlines' );
 				break;
+			case 'politics':
+				dynamic_sidebar( 'politics_headlines' );
+				break;
+			case 'entertainment':
+				dynamic_sidebar( 'entertainment_headlines' );
+				break;
+			case 'lifestyles':
+				dynamic_sidebar( 'lifestyles_headlines' );
+				break;
+			case 'columnists':
+				dynamic_sidebar( 'columnists_headlines' );
+				break;
+			case 'opinion':
+				dynamic_sidebar( 'opinion_headlines' );
+				break;
 			default:
 				dynamic_sidebar( 'news_headlines' );
 				break;
