e13cd093c8e7087ed17de45e4356a1d7e9313633
Michael Loring <mdloring@gmail.com>
2016-02-22 02:47:21 -0600

    individual taboola templates for header and content code

---
+++ parts/taboola/taboola-header.php
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+!function (e, f, u) {
+  e.async = 1;
+  e.src = u;
+  f.parentNode.insertBefore(e, f);
+}(document.createElement('script'), document.getElementsByTagName('script')[0], 'http://cdn.taboola.com/libtrc/chicagosuntimes-chicagosuntimes/loader.js');
+window.page_counter = 0;
+</script>
