dc6fe84215b36a70eca462819590521956f968c9
Michael Loring <mdloring@gmail.com>
2016-02-22 21:45:50 -0600

    base google survey js that lives in the footer. only loaded for non-logged in users and on article pages

---
+++ parts/vendors/google-survey-footer.php
@@ -0,0 +1,17 @@
+<script>
+    (function() {
+        var ARTICLE_URL = window.location.href;
+        var CONTENT_ID = 'everything';
+      document.write('<scr'+'ipt '+
+          'src="//survey.g.doubleclick.net/survey?site=_ahs3cbydcxzogxxll3gdjbxese'+
+          '&amp;url='+encodeURIComponent(ARTICLE_URL)+
+          (CONTENT_ID ? '&amp;cid='+encodeURIComponent(CONTENT_ID) : '')+
+          '&amp;random='+(new Date).getTime()+
+          '" type="text/javascript">'+'\x3C/scr'+'ipt>'
+      );
+      })()
+
+    document.addEventListener("DOMContentLoaded", function(){
+      window.GoogleSurvey && GoogleSurvey.inject()
+    })
+</script>
