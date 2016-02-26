ad2e0b02ed6a29160a9496333261b9ef7f97899d
Michael Loring <mdloring@gmail.com>
2016-02-25 22:37:43 -0600

    dfp mobile leaderboard template

---
+++ parts/dfp/homepage/dfp-mobile-leaderboard.php
@@ -0,0 +1,10 @@
+<!-- BreakingNews.Index -->
+<div id='div-gpt-mobile-leaderboard' class='dfp dfp-leaderboard dfp-centered show-for-small-only'>
+<script>
+    if ( window.innerWidth < 640 ) {
+        googletag.cmd.push(function() {
+            googletag.display('div-gpt-mobile-leaderboard');
+        });
+    }
+</script>
+</div>
