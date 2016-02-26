b3747faf78b04d9eae4f383771f6282da24cc25d
Michael Loring <mdloring@gmail.com>
2016-02-26 12:14:50 -0600

    wrap sbb in a row class

--- parts/dfp/homepage/dfp-sbb.php
+++ parts/dfp/homepage/dfp-sbb.php
@@ -1,11 +1,13 @@
+<div class="row">
 <div id='div-gpt-sbb' class='dfp dfp-sbb dfp-centered show-for-medium-up'>
 	<div id="dfp-sbb-top" class="dfp-sbb-minimize"></div>
 	<div id="dfp-sbb-bottom"></div>
 	<script>
-		if ( window.innerWidth > 640 ) {
+		if ( window.innerWidth > 970 ) {
 			googletag.cmd.push(function() {
 				googletag.display('div-gpt-sbb');
 			});
 		}
 	</script>
+</div>
 </div>
