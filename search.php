66df374febcad4e451197ababa7efc5c2c0f12a6
Michael Loring <mdloring@gmail.com>
2016-02-27 22:19:17 -0600

    add leaderboard positions to search page

--- search.php
+++ search.php
@@ -2,7 +2,11 @@
 
 <section id="search-body" class="white-background">
 	<div class="row">	
-	<div class="columns large-12">
+		<div class="columns large-12">
+			<?php
+				get_template_part( 'parts/dfp/dfp-atf-leaderboard' );
+				get_template_part( 'parts/dfp/dfp-mobile-leaderboard' ); 
+			?>
 			<script>
 				(function() {
 					var cx = '005331855116509174310:f-yelopuxim';
@@ -16,6 +20,7 @@
 				})();
 			</script>
 			<gcse:searchresults-only linktarget="_parent" queryParameterName="s"></gcse:searchresults-only>
+			<?php get_template_part( 'parts/dfp/dfp-btf-leaderboard' ); ?>
 		</div>
 	</div>
 </section>
