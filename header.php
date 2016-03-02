59324ae84dabf63ec487123e52c1b08e06820ca4
Michael Loring <mdloring@gmail.com>
2016-03-02 00:34:52 -0600

    if the post object does not return a parent section, check if it is a grandchild and pull out the parent from that

--- header.php
+++ header.php
@@ -39,6 +39,10 @@
 		$current_obj = \CST\Objects\Post::get_by_post_id( get_the_ID() );
 		if ( $current_obj ) {
 			$primary_section = $current_obj->get_primary_parent_section();
+			if( ! $primary_section ) {
+				$primary_section = $current_obj->get_grandchild_parent_section();
+			}
+			
 			if( $primary_section->slug == 'sponsored' ) {
 				get_template_part( 'parts/vendors/nativo-header' );
 				get_template_part( 'parts/vendors/nativo-content-header' );
