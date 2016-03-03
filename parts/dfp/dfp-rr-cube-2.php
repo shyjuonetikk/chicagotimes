1c728f13627d6013b7be131579d0cd55679db93a
Michael Loring <mdloring@gmail.com>
2016-03-03 10:01:46 -0600

    dfp cubes should show up on mobile as well, remove the width check

--- parts/dfp/dfp-rr-cube-2.php
+++ parts/dfp/dfp-rr-cube-2.php
@@ -1,10 +1,8 @@
 <!-- BreakingNews.Index -->
 <div id='div-gpt-rr-cube-2' class="dfp dfp-cube">
 <script>
-	if ( window.innerWidth > 640 ) {
-		googletag.cmd.push(function() {
-			googletag.display('div-gpt-rr-cube-2');
-		});
-	}
+	googletag.cmd.push(function() {
+		googletag.display('div-gpt-rr-cube-2');
+	});
 </script>
 </div>
--- parts/dfp/dfp-rr-cube-4.php
+++ parts/dfp/dfp-rr-cube-4.php
@@ -1,10 +1,8 @@
 <!-- BreakingNews.Index -->
 <div id='div-gpt-rr-cube-4' class="dfp dfp-cube">
 <script>
-	if ( window.innerWidth > 640 ) {
-		googletag.cmd.push(function() {
-			googletag.display('div-gpt-rr-cube-4');
-		});
-	}
+	googletag.cmd.push(function() {
+		googletag.display('div-gpt-rr-cube-4');
+	});
 </script>
 </div>
