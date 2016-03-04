84e56cb8a9c8fa37e7d74b509af02d094997529a
Michael Loring <mdloring@gmail.com>
2016-03-04 03:56:20 -0600

    add responsive classes to ndn and social follow us widgets

--- inc/widgets/class-cst-ndn-video-widget.php
+++ inc/widgets/class-cst-ndn-video-widget.php
@@ -16,7 +16,7 @@ class CST_NDN_Video_Widget extends WP_Widget {
 
     public function widget( $args, $instance ) {
     ?>
-    <div class="large-12 medium-6 small-6 ndn_embed">
+    <div class="large-12 medium-6 small-6 columns ndn_embed">
     <?php
         get_template_part( 'parts/dfp/homepage/ndn-video' );
     ?>
--- inc/widgets/class-cst-social-follow-us-widget.php
+++ inc/widgets/class-cst-social-follow-us-widget.php
@@ -16,7 +16,7 @@ class CST_Social_Follow_Us_Widget extends WP_Widget {
 
     public function widget( $args, $instance ) {
     ?>
-    <div class="large-12 medium-6 small-6 social-follow-us">
+    <div class="large-12 medium-6 small-12 columns social-follow-us">
     <?php
         get_template_part( 'parts/homepage/social-follow-us' );
     ?>
