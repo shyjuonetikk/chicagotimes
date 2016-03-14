69d375fd86a3434d6336f3b94918bf8fe606bde6
Michael Loring <mdloring@gmail.com>
2016-03-14 12:58:47 -0500

    add a dom listener so the politics chatter widget isn't injected until the dom has loaded

---
+++ parts/vendors/aggrego-chatter-footer.php
@@ -0,0 +1,7 @@
+<script>
+    if(window.AggregoChatter){
+        document.addEventListener("DOMContentLoaded", function(){
+            window.AggregoChatter && AggregoChatter.inject()
+        })
+    }
+</script>
