a382e84106cd6dd8d5d46ba216148b6c8346f358
Michael Loring <mdloring@gmail.com>
2016-03-14 14:35:37 -0500

    load 1 of the 3 chatter widgets, depending on the current article primary section, with politics being the default

--- assets/js/vendor/aggrego-chatter.js
+++ assets/js/vendor/aggrego-chatter.js
@@ -3,7 +3,8 @@ window.AggregoChatter = {
   /*
      Public Method for injecting Aggrego's Chatter Widget into body of the article
   */
-  inject: function(){
+  inject: function(sectionNames){
+    
     postContent = jQuery('#main').find('.cst-active-scroll-post');
     if ( ! postContent.length ) {
       return;
@@ -14,27 +15,40 @@ window.AggregoChatter = {
     if(!paragraphsCount)
       return
 
+    if(sectionNames == 'politics') {
+      var domainTag = 'politicschatter';
+      var anchorTag = 'politics';
+    } else if(sectionNames == 'entertainment') {
+      var domainTag = 'celebchatter';
+      var anchorTag = 'entertainment';
+    } else if(sectionNames == 'sports') {
+      var domainTag = 'sportschatter';
+      var anchorTag = 'sports';
+    } else {
+      var domainTag = 'politicschatter';
+      var anchorTag = 'politics';
+    }
+
     if(paragraphsCount >= 1) {
-      aggregoChatterContentNode = this._aggregoChatterHTMLTag();
+      aggregoChatterContentNode = this._aggregoChatterHTMLTag(anchorTag);
       jQuery(paragraphs[2]).append(aggregoChatterContentNode);
 
-      this._insertAggregoChatterJS();
-      
+      this._insertAggregoChatterJS(domainTag, anchorTag);      
     }
 
   },
 
   /* Private methods */
-  _insertAggregoChatterJS: function(){
+  _insertAggregoChatterJS: function(domainTag, anchorTag){
     var script = document.createElement('script');
-    script.src = "http://suntimes.politicschatter.com/widgets/2.0/collage/politics.js?anchor=politics";
+    script.src = "http://suntimes." + domainTag + ".com/widgets/2.0/collage/" + anchorTag + ".js?anchor=" + anchorTag;
     document.getElementsByTagName('head')[0].appendChild(script);
   },
 
   _aggregoChatterHTMLTag: function(tag){
 
     aggrego_chatter_div = jQuery('<div />');
-    aggrego_chatter_div.attr( 'class', 'agg-collage politics' );
+    aggrego_chatter_div.attr( 'class', 'agg-collage ' + tag );
 
     return aggrego_chatter_div;
   }
