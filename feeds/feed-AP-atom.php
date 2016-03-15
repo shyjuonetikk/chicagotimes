fca2cfaffa0daaa1883b2c6000f387a3a07425e4
Michael Loring <mdloring@gmail.com>
2016-03-15 09:21:52 -0500

    properly escape some values

--- feeds/feed-AP-atom.php
+++ feeds/feed-AP-atom.php
@@ -7,7 +7,7 @@
 
 header( 'Content-Type: ' . feed_content_type( 'atom' ) . '; charset=' . get_option( 'blog_charset' ), true );
 
-echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>';
+echo '<?xml version="1.0" encoding="' . esc_attr( get_option( 'blog_charset' ) ) . '"?' . '>';
 
 /** This action is documented in wp-includes/feed-rss2.php */
 do_action( 'rss_tag_pre', 'atom' );
@@ -23,7 +23,7 @@ do_action( 'rss_tag_pre', 'atom' );
 	<title><?php esc_attr_e( get_bloginfo_rss( 'name' ) ); ?></title>
 	<link href="<?php self_link(); ?>" rel="self"/>
 	<rights>Copyright <?php echo date( "Y" ); ?> Sun-Times Media, LLC</rights>
-	<updated><?php echo mysql2date( 'Y-m-d\TH:i:s\Z', get_lastpostmodified( 'GMT' ), false ); ?></updated>
+	<updated><?php echo esc_html( mysql2date( 'Y-m-d\TH:i:s\Z', get_lastpostmodified( 'GMT' ), false ) ); ?></updated>
 	<?php
 
 	$args = array(
@@ -36,8 +36,8 @@ do_action( 'rss_tag_pre', 'atom' );
 		<entry xml:lang="en-us">
 			<id>urn:publicid:chicago.suntimes.com:c_s_t_<?php esc_attr_e( get_the_ID() ); ?>_t</id>
 			<title><?php the_title_rss() ?></title>
-			<updated><?php echo get_post_modified_time( 'Y-m-d\TH:i:s\Z', true ); ?></updated>
-			<published><?php echo get_post_time( 'Y-m-d\TH:i:s\Z', true ); ?></published>
+			<updated><?php echo esc_html( get_post_modified_time( 'Y-m-d\TH:i:s\Z', true ) ); ?></updated>
+			<published><?php echo esc_html( get_post_time( 'Y-m-d\TH:i:s\Z', true ) ); ?></published>
 			<rights>Copyright <?php echo date( "Y" ); ?> Sun-Times Media, LLC</rights>
 			<link href="<?php the_permalink_rss() ?>" rel="alternate"/>
 			<summary type="xhtml">
@@ -58,7 +58,7 @@ do_action( 'rss_tag_pre', 'atom' );
 				<?php $content = get_post_field( 'post_content', $post->ID );
 				$word_count    = str_word_count( strip_tags( $content ) ); ?>
 				<apcm:Characteristics MediaType="Text"
-				/ Words="<?php echo $word_count; ?>">
+				/ Words="<?php echo esc_attr( $word_count ); ?>">
 			</apcm:ContentMetadata>
 		</entry>
 	<?php endwhile; ?>
