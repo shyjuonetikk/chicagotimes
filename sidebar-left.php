a220ee1a7f58891ce5b7a046417aa7cabc36bdf4
Michael Loring <mdloring@gmail.com>
2016-03-16 17:40:14 -0500

    left and right sidebars

---
+++ sidebar-left.php
@@ -0,0 +1,3 @@
+<aside class="sidebar show-for-large-up" id="post-sidebar" style="display: none !important;">
+    <?php get_template_part( 'parts/dfp/dfp-rr-cube-1' ); ?>
+</aside>
---
+++ sidebar-right.php
@@ -0,0 +1,3 @@
+<aside class="sidebar show-for-large-up" id="post-sidebar-right">
+    <?php //get_template_part( 'parts/dfp/dfp-rr-cube-1' ); ?>
+</aside>
