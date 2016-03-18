5ee389454d18e1e2cb3cad0dd0fae5417d887e5c
Michael Loring <mdloring@gmail.com>
2016-03-18 11:11:46 -0500

    add widget class so the sidebar widget styling displays properly

--- sidebar-right.php
+++ sidebar-right.php
@@ -1,3 +1,3 @@
-<aside class="sidebar show-for-large-up article-sidebar" id="post-sidebar">
+<aside class="sidebar show-for-large-up article-sidebar widgets" id="post-sidebar">
     <?php if( dynamic_sidebar( 'articleright' ) ) { } ?>
 </aside>
