822ca3aac3e29ffb545f518e7fb3004a90b50cb1
Michael Loring <mdloring@gmail.com>
2016-03-18 10:37:25 -0500

    article sidebar templates

--- sidebar-left.php
+++ sidebar-left.php
@@ -1,3 +1,3 @@
-<aside class="sidebar show-for-large-up" id="post-sidebar" style="display: none !important;">
-    <?php get_template_part( 'parts/dfp/dfp-rr-cube-1' ); ?>
+<aside class="sidebar show-for-large-up article-sidebar" id="post-sidebar-left">
+    
 </aside>
--- sidebar-right.php
+++ sidebar-right.php
@@ -1,3 +1,3 @@
-<aside class="sidebar show-for-large-up" id="post-sidebar-right">
-    <?php //get_template_part( 'parts/dfp/dfp-rr-cube-1' ); ?>
+<aside class="sidebar show-for-large-up article-sidebar" id="post-sidebar">
+    <?php if( dynamic_sidebar( 'articleright' ) ) { } ?>
 </aside>
