09f764dd88fbae308e31dcf8831146ffb44d4f77
Michael Loring <mdloring@gmail.com>
2016-03-21 11:49:48 -0500

    cache the json response for 5mins

--- inc/widgets/class-cst-chartbeat-users-currently-viewing-widget.php
+++ inc/widgets/class-cst-chartbeat-users-currently-viewing-widget.php
@@ -20,27 +20,29 @@ class CST_Chartbeat_Currently_Viewing_Widget extends WP_Widget {
 
         echo $args['before_title'] . esc_html( 'Users Currently Viewing' ) . $args['after_title'];
 
-        $response = wpcom_vip_file_get_contents( 'http://api.chartbeat.com/live/toppages/v3/?apikey=' . CST_CHARTBEAT_API_KEY . '&host=chicago.suntimes.com&section=news,%20sports,%20entertainment,%20lifestyles,%20columnists,%20politics&all_platforms=1&types=0&limit=5
-' );
-        $result = json_decode( $response );
-        if( $result ) {
+        $feed_url = 'http://api.chartbeat.com/live/toppages/v3/?apikey=' . CST_CHARTBEAT_API_KEY . '&host=chicago.suntimes.com&section=news,%20sports,%20entertainment,%20lifestyles,%20columnists,%20politics&all_platforms=1&types=0&limit=5
+';
+        $cache_key = md5( $feed_url );
+        $result = wp_cache_get( $cache_key, 'default' ); //VIP: for some reason fetch_feed is not caching this properly.
+        if ( $result === false ) {
+            $response = wpcom_vip_file_get_contents( $feed_url );
+            if ( ! is_wp_error( $response ) ) {
+                $result = json_decode( $response );
+                wp_cache_set( $cache_key, $result, 'default', 5 * MINUTE_IN_SECONDS );
+            }
+        }
         ?>
         <ul class="widget-chartbeat-currently-viewing">
-        <?php
-            foreach( $result->pages as $item ) {
-        ?>
+        <?php foreach( $result->pages as $item ) { ?>
             <li>
                 <a href="<?php echo esc_url( $item->path ); ?>">
                 <span class='section'><?php echo esc_html( $item->sections[0] ) ?></span><br/>
                 <span class='title'><?php echo esc_html( $item->title ); ?></span>
                 </a>
             </li>
-        <?php
-            }
-        ?>
+        <?php } ?>
         </ul>
         <?php
-        }
         echo $args['after_widget'];
     }
 
