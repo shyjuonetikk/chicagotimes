cfb2a42a1420b931c7af61ca3a3747b9c176910b
Sean Hayes <seanchayes@gmail.com>
2016-03-28 08:52:08 -0500

    WIP for author page improvements to take care of guest authors as well as real authors #53

--- parts/section/author-top.php
+++ parts/section/author-top.php
@@ -2,15 +2,31 @@
 	<?php get_template_part( 'parts/images/main-site-logo'); ?>
 </a>
 <?php $author = new \CST\Objects\User( get_queried_object_id() ) ?>
-
+<?php
+if ( null === $author->get_id() ) {
+	global $coauthors_plus, $author_name;
+	$author = $coauthors_plus->get_coauthor_by( 'login', $author_name );
+	$author_avatar_html = $coauthors_plus->guest_authors->get_guest_author_thumbnail( $author );
+	$author_display_name = $author->display_name;
+	$author_description = $author->description;
+	$author_fields = $coauthors_plus->guest_authors->get_guest_author_fields( 'social' );
+	$author_twitter = $coauthors_plus->guest_authors->get_post_meta_key( 'twitter' );
+	
+} else {
+	$author_avatar_html = $author->get_avatar( 80 );
+	$author_display_name = $author->get_display_name();
+	$author_description = $author->get_description();
+	$author_twitter = $author->get_twitter_username();
+}
+?>
 <div id="section-top" class="author">
 	<div class="row">
 		<div class="small-2 columns">
-			<?php echo $author->get_avatar( 80 ); ?>
+			<?php echo $author_avatar_html; ?>
 		</div>
 		<div class="small-10 columns">
 			<h2>
-				<?php echo esc_html( $author->get_display_name() ); ?>
+				<?php echo esc_html( $author_display_name ); ?>
 			</h2>
 			<p>
 				<?php if ( $username = $author->get_twitter_username() ) : ?>
@@ -29,7 +45,7 @@
 				<?php endif; ?>
 			</p>
 		</div>
-		<?php if ( $description = $author->get_description() ) : ?>
+		<?php if ( $description = $author_description ) : ?>
 		<div class="small-12 columns">
 			 <small class="author-bio"><?php echo wp_kses_post( wpautop( $description ) ); ?></small>
 		</div>
