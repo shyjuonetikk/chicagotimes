82a5adf61ec4461a123aad5f2b95bb92580fdccb
Michael Loring <mdloring@gmail.com>
2016-03-31 13:17:49 -0500

    load the nativo article template

--- sidebar-left.php
+++ sidebar-left.php
@@ -3,6 +3,5 @@
 		<img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=160%C3%97600&w=160&h=600" alt="">
 		<hr>
     <?php get_template_part( 'parts/vendors/nativo-article' ); ?>
-		<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/example-nativo-widget-02.jpg' ); ?>" alt="">
 	</div>
 </aside>
