1a4dbc3e03a964ec86799b01446be6262ed2b409
Michael Loring <mdloring@gmail.com>
2016-03-31 12:07:06 -0500

    determine the nativo sponsored positions based on the first article primary parent slug

--- inc/class-cst-frontend.php
+++ inc/class-cst-frontend.php
@@ -901,4 +901,83 @@ class CST_Frontend {
 		}
 		echo $cached_content;
 	}
+
+	public function cst_nativo_determine_positions($slug) {
+
+        $positions = array();
+        switch( $slug ) {
+
+            case 'news':
+                $positions = array( 'News1', 'News2' );
+                break;
+            case 'chicago':
+                $positions = array('NewsChi1', 'NewsChi2' );
+                break;
+            case 'crime':
+                $positions = array( 'NewsCrime1', 'NewsCrime2' );
+                break;
+            case 'the-watchdogs':
+                $positions = array( 'NewsWatch1', 'NewsWatch2' );
+                break;
+            case 'nation-world':
+                $positions = array( 'NewsNation1', 'NewsNation2' );
+                break;
+            case 'education':
+                $positions = array( 'NewsEdu1', 'NewsEdu2' );
+                break;
+            case 'transportation':
+                $positions = array( 'NewsTrans1', 'NewsTrans2' );
+                break;
+            case 'business':
+                $positions = array( 'NewsBus1', 'NewsBus2' );
+                break;
+            case 'sneed':
+                $positions = array( 'NewsSneed1', 'NewsSneed2' );
+                break;
+            case 'chicago-politics':
+                $positions = array( 'PolChi1', 'PolChi2' );
+                break;
+            case 'springfield-politics':
+                $positions = array( 'PolSpring1', 'PolSpring2' );
+                break;
+            case 'washington-politics':
+                $positions = array( 'PolWash1', 'PolWash2' );
+                break;
+            case 'lynn-sweet-politics':
+                $positions = array( 'PolSweet1', 'PolSweet2' );
+                break;
+            case 'rick-morrissey':
+                $positions = array( 'SportsMorrissey1', 'SportsMorrissey2' );
+                break;
+            case 'rick-telander':
+                $positions = array( 'SportsTelander1', 'SportsTelander2' );
+                break;
+            case 'cubs-baseball':
+                $positions = array( 'SportsCubs1', 'SportsCubs2' );
+                break;
+            case 'white-sox':
+                $positions = array( 'SportsSox1', 'SportsSox2' );
+                break;
+            case 'bears':
+                $positions = array( 'SportsBears1', 'SportsBears2' );
+                break;
+            case 'blackhawks':
+                $positions = array( 'SportsHawks1', 'SportsHawks2' );
+                break;
+            case 'bulls':
+                $positions = array( 'SportsBulls1', 'SportsBulls2' );
+                break;
+            case 'outdoor':
+                $positions = array( 'SportsOutdoor1', 'SportsOutdoor2' );
+                break;
+            case 'fire':  
+                $positions = array( 'SportsFire1', 'SportsFire2' );
+                break;
+            case 'colleges':
+                $positions = array( 'SportsColleges1', 'SportsColleges2' );
+                break;
+        }
+        return $positions;
+    }
+
 }
