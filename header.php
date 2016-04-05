718c26a9e984f42ea1849dc14c34db5121ec6bd1
Michael Loring <mdloring@gmail.com>
2016-04-05 12:25:10 -0500

    pulling in VIP SVN updates

--- header.php
+++ header.php
@@ -11,7 +11,8 @@
 <head>
 	<?php get_template_part( 'parts/analytics/chartbeat-header' ); ?>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
