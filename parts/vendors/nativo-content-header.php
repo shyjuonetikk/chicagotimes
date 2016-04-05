132bb4f9bab3e5bfe21081b2fdd65d5d0f7cb70b
Sean Hayes <seanchayes@gmail.com>
2016-04-05 11:51:20 -0500

    Moving http-equiv meta to header

--- parts/vendors/nativo-content-header.php
+++ parts/vendors/nativo-content-header.php
@@ -1,2 +1,2 @@
 <script type="text/javascript" src="//s.ntv.io/serve/load.js" async></script>
-<meta http-equiv="X-UA-Compatible" content="IE=10" /> <meta name="robots" content="noindex, nofollow" />
+<meta name="robots" content="noindex, nofollow" />
