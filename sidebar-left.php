24761b713c4b40a4601ebbf276ad30d67e25ba90
Michael Loring <mdloring@gmail.com>
2016-04-06 15:44:49 -0500

    article page sidebars

--- sidebar-left.php
+++ sidebar-left.php
@@ -1,7 +1,9 @@
 <aside class="sidebar show-for-large-up article-sidebar" id="post-sidebar-left">
-	<div class="stick-sidebar-left">
-		<img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=160%C3%97600&w=160&h=600" alt="">
-		<hr>
+<?php if( is_single() ) : ?>
+    <div class="stick-sidebar-left">
+    <?php get_template_part( 'parts/dfp/dfp-sky-scraper' ); ?>
+        <hr>
     <?php get_template_part( 'parts/vendors/nativo-article' ); ?>
-	</div>
+    </div>
+<?php endif; ?>
 </aside>
--- sidebar-right.php
+++ sidebar-right.php
@@ -1,3 +1,5 @@
 <aside class="sidebar show-for-large-up article-sidebar widgets" id="post-sidebar">
+<?php if( is_single() ) : ?>
     <?php if( dynamic_sidebar( 'articleright' ) ) { } ?>
+<?php endif; ?>
 </aside>
