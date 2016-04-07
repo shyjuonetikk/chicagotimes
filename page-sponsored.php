4ebd02b6b140a10ee541fe5a8e2291e4afd0bc65
Michael Loring <mdloring@gmail.com>
2016-04-07 13:25:19 -0500

    sponsored nativo article page template

---
+++ page-sponsored.php
@@ -0,0 +1,53 @@
+<?php
+  /* 
+   * Template Name: Nativo Article Page
+   */
+?>
+<?php get_header(); ?>
+
+  <?php get_sidebar( 'left' ); ?>
+
+  <section id="post-body">
+
+    <div class="row">
+
+      <div id="main" class="columns large-10 end">
+
+        <div class="row post-row"> 
+
+          <article id="post-21370" class="post-21370 cst_article type-cst_article status-publish has-post-thumbnail hentry single-view columns large-12 end cst-sharing-relative post active cst-active-scroll-post" data-cst-post-id="21370" data-cst-post-uri="/news/byline-article-test/"> 
+        
+        <div class="post-meta post-meta-top columns medium-11 medium-offset-1 end" style="margin-top:20px;">
+          <span class="post-section-taxonomy"><a href="http://cst.dev/blog/section/news/">Sponsored</a></span>
+          <span class="post-relative-date top-date"></span>
+        </div>
+        <div class="columns medium-11 medium-offset-1 end">
+          <h1></h1>
+        </div>
+        
+        <div class="post-lead-media columns medium-11 medium-offset-1 end">
+          <img width="635" height="414" src="" class="attachment-cst-article-featured">        
+          <div class="image-caption">
+          </div>
+        </div>
+        <div class="post-meta post-meta-byline columns medium-11 medium-offset-1 end">
+          <div class="post-meta-author"><a href="#"></a></div>
+          <div class="post-meta-author-contact"><a href="#"></a> | <a href="#"></a></div> 
+        </div>
+        <div class="post-content columns medium-9 medium-offset-1 end">
+
+        </div>
+    
+      </article>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <?php get_sidebar( 'right' ); ?>
+
+<?php get_footer(); ?>
