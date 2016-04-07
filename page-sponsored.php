cae3d9d82ef6dba5b0848508b51865bb703678b4
Michael Loring <mdloring@gmail.com>
2016-04-07 13:56:35 -0500

    sponsored link should link to the sponsored page, not the news page

--- page-sponsored.php
+++ page-sponsored.php
@@ -18,7 +18,7 @@
           <article id="post-21370" class="post-21370 cst_article type-cst_article status-publish has-post-thumbnail hentry single-view columns large-12 end cst-sharing-relative post active cst-active-scroll-post" data-cst-post-id="21370" data-cst-post-uri="/news/byline-article-test/"> 
         
         <div class="post-meta post-meta-top columns medium-11 medium-offset-1 end" style="margin-top:20px;">
-          <span class="post-section-taxonomy"><a href="http://cst.dev/blog/section/news/">Sponsored</a></span>
+          <span class="post-section-taxonomy"><a href="<?php echo esc_url( home_url( '/sponsored' ) ); ?>">Sponsored</a></span>
           <span class="post-relative-date top-date"></span>
         </div>
         <div class="columns medium-11 medium-offset-1 end">
