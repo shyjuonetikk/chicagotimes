69081472f1e4be6815c5c3f0a30c357942ec153b
Michael Loring <mdloring@gmail.com>
2016-04-13 12:37:01 -0500

    homepage more well nativo position

---
+++ parts/vendors/nativo-home-1.php
@@ -0,0 +1,14 @@
+<hr/>
+<div class="row" id="Home1">
+    <div class="large-4 medium-4 small-12 columns article-image">
+        <a href="">
+        </a>
+    </div>
+    <div class="large-8 medium-8 small-12 columns">
+        <a href="">
+            <h3></h3>
+        </a>
+        <p></p>
+        <span class="author"></span>
+    </div>
+</div>
