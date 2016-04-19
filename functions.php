02281321b5a8356701f0226b86cee7905a4d1ddb
Sean Hayes <seanchayes@gmail.com>
2016-04-19 09:35:34 -0500

    Removing gallery markup and styling from loading on 404 page, adding in basic js for foundation to trigger navigation functionality

--- functions.php
+++ functions.php
@@ -1230,6 +1230,9 @@ class CST {
 	 * Add the gallery backdrop to the footer
 	 */
 	public function action_wp_footer_gallery_backdrop() {
+		if ( is_404() ) {
+			return;
+		}
 		echo $this->get_template_part( 'post/gallery-backdrop' );
 	}
 
