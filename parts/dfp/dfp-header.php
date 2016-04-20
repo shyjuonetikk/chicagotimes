7926274ec394784c5d43cff00713fbebc0567fbc
Michael Loring <mdloring@gmail.com>
2016-04-20 14:23:04 -0500

    load the dfp promo cube on section fronts too

--- parts/dfp/dfp-header.php
+++ parts/dfp/dfp-header.php
@@ -85,6 +85,10 @@
 		.addService(googletag.pubads())
 		.setTargeting("pos","rr cube 6");
 
+        CSTAdTags['div-gpt-rr-cube-promo'] = googletag.defineSlot(<?php echo wp_json_encode('/61924087/chicago.suntimes.com/chicago.suntimes.com.' . $dfp_slug . '/chicago.suntimes.com.' . $dfp_slug . '.index'); ?>, [300, 250], 'div-gpt-rr-cube-promo')
+        .addService(googletag.pubads())
+        .setTargeting("pos","rr cube promo");
+
 		CSTAdTags['div-gpt-billboard'] = googletag.defineSlot(<?php echo wp_json_encode('/61924087/chicago.suntimes.com/chicago.suntimes.com.' . $dfp_slug . '/chicago.suntimes.com.' . $dfp_slug . '.index'); ?>, [970, 250], 'div-gpt-billboard')
         .addService(googletag.pubads())
         .setTargeting("pos","Billboard 970x250")
