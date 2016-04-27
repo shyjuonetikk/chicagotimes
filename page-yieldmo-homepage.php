395f85a62e83e8b032775185a0b7d3a68bdd0993
Michael Loring <mdloring@gmail.com>
2016-04-27 14:26:45 -0500

    setup yieldmo based homepage template copy

---
+++ page-yieldmo-homepage.php
@@ -0,0 +1,43 @@
+<?php
+    /*
+     * Template Name: YieldMo Homepage
+     */
+ ?>
+<?php get_header(); ?>
+<?php get_template_part( 'parts/dfp/homepage/dfp-wallpaper' ); ?>
+            <?php 
+                if ( is_active_sidebar( 'homepage_breaking_news' ) ) :
+                    dynamic_sidebar( 'homepage_breaking_news' ); 
+                endif;
+            ?>
+            <div class="row">
+                <div class="large-12 content-wrapper">
+                        <div class="row">
+                            <div class="large-12 columns dfp-atf-leaderboard show-for-medium-up">
+                                <?php get_template_part( 'parts/dfp/homepage/dfp-atf-leaderboard' ); ?>
+                            </div>
+                            <div class="large-12 columns dfp-mobile-leaderboard show-for-small-only">
+                                <?php get_template_part( 'parts/dfp/homepage/dfp-mobile-leaderboard' ); ?>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <?php 
+            do_action( 'above-homepage-headlines' );
+                if ( is_active_sidebar( 'homepage_headlines' ) ) :
+                    dynamic_sidebar( 'homepage_headlines' ); 
+                endif;
+            ?>
+            <div class="row">
+                <div class="large-12 columns content-wrapper">
+                    <?php 
+                        if ( is_active_sidebar( 'homepage_featured_story' ) ) :
+                            dynamic_sidebar( 'homepage_featured_story' ); 
+                        endif;
+                    ?>
+                    <?php get_template_part( 'parts/homepage/column-wells' ); ?>
+                </div>
+            </div>
+        <?php get_template_part( 'parts/homepage/footer' ); ?>
+</div>
+<?php get_footer(); ?>
