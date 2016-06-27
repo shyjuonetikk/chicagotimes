57adb46d2b96900f3d157d6f5e432dd786ff6e19
Michael Loring <mdloring@gmail.com>
2016-06-27 12:22:10 -0500

    bring in fieldmanager fields for wssp subscribe pages. re-structure subscribe page template to match new styling

--- page-subscribe.php
+++ page-subscribe.php
@@ -4,18 +4,31 @@
    */
 ?>
 <?php get_header(); ?>
-
-<?php get_template_part( 'parts/homepage/navigation' ); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php $obj = \CST\Objects\Page::get_by_post_id( get_the_ID() ); ?>
+<?php if( $obj ) : ?>
+	<?php 
+		$subscribe_content = $obj->get_subscribe_title_content();
+		$subscribe_print_content = $obj->get_subscribe_print_package();
+		$subscribe_digital_content = $obj->get_subscribe_digital_package();
+	?>
 <div>
 	<section id="subscribe">
 		<div class="row">
 			<div class="just-in-wrapper large-12 columns mbox3">
 					<div class="small-12 columns">
 							<div class="small-12 medium-4 columns">
-								<h1>Subscribe Today!</h1>
+							<?php if( ! empty( $subscribe_content['content']['top_left'] ) ) { ?>
+								<h1><?php esc_html_e( $subscribe_content['content']['top_left'], 'chicagosuntimes' ); ?></h1>
+							<?php } ?>
+							<?php if( ! empty( $subscribe_content['content']['image'] ) ) { ?>
+									<img src="<?php echo esc_url( $obj->get_subscribe_image_url( $subscribe_content['content']['image'] ) ); ?>" />
+							<?php } ?>
 							</div>
 							<div class="small-12 medium-8 columns left-pad">
-								<p class="floatiesL">The Chicago Sun-Times continues to bring you news on everything from politics and crime, to entertainment and movies, plus Chicago's best sports coverage. <strong>Don't miss a day!</strong></p>
+					<?php if( ! empty( $subscribe_content['content']['top_right'] ) ) { ?>
+								<p class="floatiesL"><?php esc_html_e( $subscribe_content['content']['top_right'], 'chicagosuntimes' ); ?></p>
+					<?php } ?>
 							</div>
 					</div>
 			</div>
@@ -24,93 +37,142 @@
 				<div class="small-12 columns back">
 					<div class="medium-4 columns right-border">
 						<div class="small-centered large-uncentered columns">
-							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/printdig-washpost-subscribe1014.png" width="336" />
+							<?php 
+							if ( $obj->get_featured_image_id() ) {
+								echo $obj->get_featured_image_html(); 
+							}
+							?>
 						</div>
 					</div>
+
 					<div class="medium-8 columns left-pad">
+		<?php if( ! empty( $subscribe_print_content['print_package_1']['package_title'] ) ) { ?>
 						<div class="small-12 columns lower">
 							<div class="small-12 medium-8 columns details">
-								<h3>Sunday Only</h3>
-								<p>Enjoy Sunday only delivery including a dose of society and fashion in Splash magazine plus sales ads, coupons and more!</p>
+								<h3><?php esc_html_e( $subscribe_print_content['print_package_1']['package_title'], 'chicagosuntimes' ); ?></h3>
+								<p><?php esc_html_e( $subscribe_print_content['print_package_1']['package_description'], 'chicagosuntimes' ); ?></p>
 							</div>
 							<div class="small-12 medium-4 columns pricing">
 								<div>
-									<h2><sup>$</sup>5.99<span class="month-style">/month</span></h2>
-									<span><a href="https://suntiservices.dticloud.com/cgi-bin/cmo_cmo.sh/custservice/web/addrfind.html?siteid=CST&amp;campaign=SXSU0599A"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/promo-button-selectoffer.png" ></a></span>
-									<br>
-									<span><a href="http://bitcoin.suntimes.com"><img class="bitcoin1" src="<?php echo get_template_directory_uri(); ?>/assets/images/subscribe-bitcoin.png"></a></span>
+									<h2><sup>$</sup><?php esc_html_e( $subscribe_print_content['print_package_1']['package_price'], 'chicagosuntimes' ); ?></h2>
+									<span><a href="<?php esc_html_e( $subscribe_print_content['print_package_1']['package_url'], 'chicagosuntimes' ); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/promo-button-selectoffer.png" ></a></span>
 								</div>
 							</div>
 						</div>
+		<?php } ?>
 
+		<?php if( ! empty( $subscribe_print_content['print_package_2']['package_title'] ) ) { ?>
 						<div class="small-12 columns lower">
-							<div class="small-12 medium-8 columns">
-								<h3>Wed, Thu, Fri and Sunday</h3>
-								<p>Read up on food and recipes every Wednesday in Taste, entertainment and movies every Friday in Agenda, fashion and society in Splash every Sunday plus sales ads, coupons and more!</p>
-
+							<div class="small-12 medium-8 columns details">
+								<h3><?php esc_html_e( $subscribe_print_content['print_package_2']['package_title'], 'chicagosuntimes' ); ?></h3>
+								<p><?php esc_html_e( $subscribe_print_content['print_package_2']['package_description'], 'chicagosuntimes' ); ?></p>
 							</div>
 							<div class="small-12 medium-4 columns pricing">
 								<div>
-									<h2><sup>$</sup>11.99<span class="month-style">/month</span></h2>
-									<span><a href="https://suntiservices.dticloud.com/cgi-bin/cmo_cmo.sh/custservice/web/addrfind.html?siteid=CST&amp;campaign=SXWYP1199A"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/promo-button-selectoffer.png" ></a></span>
-									<br>
-									<span><a href="http://bitcoin.suntimes.com"><img class="bitcoin1" src="<?php echo get_template_directory_uri(); ?>/assets/images/subscribe-bitcoin.png"></a></span>
+									<h2><sup>$</sup><?php esc_html_e( $subscribe_print_content['print_package_2']['package_price'], 'chicagosuntimes' ); ?></h2>
+									<span><a href="<?php esc_html_e( $subscribe_print_content['print_package_2']['package_url'], 'chicagosuntimes' ); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/promo-button-selectoffer.png" ></a></span>
 								</div>
 							</div>
 						</div>
+		<?php } ?>
 
+		<?php if( ! empty( $subscribe_print_content['print_package_3']['package_title'] ) ) { ?>
 						<div class="small-12 columns">
 							<div class="small-12 medium-8 columns details">
-								<h3>Daily Delivery (Monday - Sunday)</h3>
-								<p>Don't miss a day. Your complete source for hard-hitting local and in-depth news, award-winning journalism and Chicago's best sports coverage... 7 days a week!</p>
-
+								<h3><?php esc_html_e( $subscribe_print_content['print_package_3']['package_title'], 'chicagosuntimes' ); ?></h3>
+								<p><?php esc_html_e( $subscribe_print_content['print_package_3']['package_description'], 'chicagosuntimes' ); ?></p>
 							</div>
 							<div class="small-12 medium-4 columns pricing">
 								<div class="last-price">
-									<div>&nbsp;<!--Bitcoin necessary spacer --></div>
-									<h2><sup>$</sup>18.99<span class="month-style">/month</span></h2>
-									<span><a href="https://suntiservices.dticloud.com/cgi-bin/cmo_cmo.sh/custservice/web/addrfind.html?siteid=CST&amp;campaign=SXDSP1899A"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/promo-button-selectoffer.png" ></a></span>
-									<br>
-									<span><a href="http://bitcoin.suntimes.com"><img class="bitcoin2" src="<?php echo get_template_directory_uri(); ?>/assets/images/subscribe-bitcoin.png"></a></span>
+									<h2><sup>$</sup><?php esc_html_e( $subscribe_print_content['print_package_3']['package_price'], 'chicagosuntimes' ); ?></h2>
+									<span><a href="<?php esc_html_e( $subscribe_print_content['print_package_3']['package_url'], 'chicagosuntimes' ); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/promo-button-selectoffer.png" ></a></span>
 								</div>
 							</div>
 						</div>
-
+		<?php } ?>
 					</div>
 				</div>
 				<div class="small-12 columns back">
-					<p class="benefit"><i>Benefit available through an agreement with The Washington Post and is subject to change or cancellation at any time without prior notice. Benefit available to current print subscribers to the Chicago Sun-Times and is non-transferable. Limit one free Washington Post Digital Premium subscription per person. Additional restrictions may apply.</i></p>
+			<?php if( ! empty( $subscribe_content['content']['benefits'] ) ) { ?>
+					<p class="benefit"><i><?php esc_html_e( $subscribe_content['content']['benefits'] ); ?></i></p>
+			<?php } ?>
 				</div>
 				<div class="small-12 columns spacer">
 
 				</div>
+
+		<?php if( ! empty( $subscribe_digital_content['digital_package_1']['package_title'] ) ) { ?>
 				<div class="small-12 columns back">
 					<div class="small-12 medium-4 columns right-border">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/subscribeCST-digital.png"/>
+						<h2 class="subscribe-product">Digital</h2>
+						<?php if( ! empty( $subscribe_digital_content['digital_package_1']['image'] ) ) { ?>
+								<img src="<?php echo esc_url( $obj->get_subscribe_image_url( $subscribe_digital_content['digital_package_1']['image'] ) ); ?>" />
+						<?php } ?>
 					</div>
 					<div class="small-12 medium-8 columns left-pad">
 						<div class="small-12 columns">
 							<div class="small-12 medium-8 columns details">
 
-								<h3>Today's Paper (Digital only)</h3>
-								<p>Prefer to read the printed newspaper in a digital format? Today's Paper allows you to read your newspaper page-by-page just like the print version, but in a digital format. It's accessible on any desktop computer, laptop or iOS device with an internet connection, from anywhere in the world!</p>
+								<h3><?php esc_html_e( $subscribe_digital_content['digital_package_1']['package_title'], 'chicagosuntimes' ); ?></h3>
+								<p><?php esc_html_e( $subscribe_digital_content['digital_package_1']['package_description'], 'chicagosuntimes' ); ?></p>
 
 							</div>
 							<div class="small-12 medium-4 columns">
-								<h2><sup>$</sup>3.99<span class="month-style">/month</span></h2>
-								<span><a href="https://suntiservices.dticloud.com/cgi-bin/cmo_cmo.sh/custservice/web/addrfind.html?siteid=CST&amp;campaign=SXDSO0399A"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/promo-button-selectoffer.png" ></a></span>
-								<br>
-								<span><a href="http://bitcoin.suntimes.com"><img class="bitcoin" src="<?php echo get_template_directory_uri(); ?>/assets/images/subscribe-bitcoin.png"></a></span>
+								<h2><sup>$</sup><?php esc_html_e( $subscribe_digital_content['digital_package_1']['package_price'], 'chicagosuntimes' ); ?></h2>
+								<span><a href="<?php esc_html_e( $subscribe_digital_content['digital_package_1']['package_url'], 'chicagosuntimes' ); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/promo-button-selectoffer.png"></a></span>
 							</div>
 						</div>
 					</div>
 				</div>
+		<?php } ?>
+
+		<?php if( ! empty( $subscribe_digital_content['digital_package_2']['package_title'] ) ) { ?>
+				<div class="small-12 columns back">
+					<div class="small-12 medium-8 columns left-pad">
+						<div class="small-12 columns">
+							<div class="small-12 medium-8 columns details">
+
+								<h3><?php esc_html_e( $subscribe_digital_content['digital_package_2']['package_title'], 'chicagosuntimes' ); ?></h3>
+								<p><?php esc_html_e( $subscribe_digital_content['digital_package_2']['package_description'], 'chicagosuntimes' ); ?></p>
+
+							</div>
+							<div class="small-12 medium-4 columns">
+								<h2><sup>$</sup><?php esc_html_e( $subscribe_digital_content['digital_package_2']['package_price'], 'chicagosuntimes' ); ?></h2>
+								<span><a href="<?php esc_html_e( $subscribe_digital_content['digital_package_2']['package_url'], 'chicagosuntimes' ); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/promo-button-selectoffer.png"></a></span>
+							</div>
+						</div>
+					</div>
+				</div>
+		<?php } ?>
+
+		<?php if( ! empty( $subscribe_digital_content['digital_package_3']['package_title'] ) ) { ?>
+				<div class="small-12 columns back">
+					<div class="small-12 medium-8 columns left-pad">
+						<div class="small-12 columns">
+							<div class="small-12 medium-8 columns details">
+
+								<h3><?php esc_html_e( $subscribe_digital_content['digital_package_3']['package_title'], 'chicagosuntimes' ); ?></h3>
+								<p><?php esc_html_e( $subscribe_digital_content['digital_package_3']['package_description'], 'chicagosuntimes' ); ?></p>
+
+							</div>
+							<div class="small-12 medium-4 columns">
+								<h2><sup>$</sup><?php esc_html_e( $subscribe_digital_content['digital_package_3']['package_price'], 'chicagosuntimes' ); ?></h2>
+								<span><a href="<?php esc_html_e( $subscribe_digital_content['digital_package_3']['package_url'], 'chicagosuntimes' ); ?>" target="_blank"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/promo-button-selectoffer.png"></a></span>
+							</div>
+						</div>
+					</div>
+				</div>
+		<?php } ?>
 
 			</div>
 
 		</div>
 	</section>
 </div>
+<?php endif; ?>
+<?php endwhile; else : ?>
+						<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+					<?php endif; ?>
 <?php get_template_part( 'parts/homepage/footer' ); ?>
 
 <?php get_footer(); ?>
