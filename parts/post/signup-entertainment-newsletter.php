704e832dcf7e5431aa55ba62f039920554c625aa
Michael Loring <mdloring@gmail.com>
2016-07-20 16:13:17 -0500

    restructure html

--- parts/post/signup-entertainment-newsletter.php
+++ parts/post/signup-entertainment-newsletter.php
@@ -3,7 +3,7 @@
     <span>
         <?php esc_html_e( 'Receive Theatre, Celebrity & Entertainment related Updates' ); ?>
     </span>
-    <a href="<?php echo esc_url( 'https://r1.surveysandforms.com/062jcp97-bf1s1y92' ); ?>" target="_blank" class="button tiny info">
+    <a href="<?php echo esc_url( 'https://r1.surveysandforms.com/062jcp97-bf1s1y92' ); ?>" target="_blank" class="button tiny success">
         <i class="fa fa-envelope"></i> Sign-Up
     </a>
 </div>
