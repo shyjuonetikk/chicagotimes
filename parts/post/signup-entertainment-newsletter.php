6c2734478492fc337e146b4447376ccba4e37071
Michael Loring <mdloring@gmail.com>
2016-07-22 16:12:23 -0500

    part of VIPs revert

--- parts/post/signup-entertainment-newsletter.php
+++
@@ -1,9 +0,0 @@
-<div class="large-12 columns entertainment-sign-up">
-    <h3>Sign-Up for Chicago Sun-Times Entertainment Newsletter</h3>
-    <span>
-        <?php esc_html_e( 'Receive Theatre, Celebrity & Entertainment related Updates' ); ?>
-    </span>
-    <a href="<?php echo esc_url( 'https://r1.surveysandforms.com/062jcp97-bf1s1y92' ); ?>" target="_blank" class="button tiny success">
-        <i class="fa fa-envelope"></i> Sign-Up
-    </a>
-</div>
