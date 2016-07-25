69d318de2061ac3ed6ffac4a96e4bc09a900493d
Sean Hayes <seanchayes@gmail.com>
2016-07-25 14:21:51 -0500

    WIP for Chartbeat analytics - we are passing them the post id with this modification and we should see that in the returned toppages api call

--- parts/analytics/chartbeat-footer.php
+++ parts/analytics/chartbeat-footer.php
@@ -27,6 +27,10 @@ _sf_async_config.useCanonical = true;
         _sf_async_config.authors = '<?php echo esc_js( $author->get_display_name() ); ?>';
     <?php endif; ?>
 <?php endif; ?>
+<?php if ( is_singular() ) { ?>
+var _cbq = window._cbq || [];
+_cbq.push(['_postid','<?php echo get_the_ID(); ?>']);
+<?php } ?>
 (function(){
 function loadChartbeat()
 { window._sf_endpt=(new Date()).getTime(); var e = document.createElement('script'); e.setAttribute('language', 'javascript'); e.setAttribute('type', 'text/javascript'); e.setAttribute('src', '//static.chartbeat.com/js/chartbeat.js'); document.body.appendChild(e); }
