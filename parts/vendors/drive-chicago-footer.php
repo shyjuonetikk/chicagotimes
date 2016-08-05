beb17c6774e96eecdd81627c82e4354a12b8cc5f
Sean Hayes <seanchayes@gmail.com>
2016-08-05 11:24:20 -0500

    Revert "Avoid duplication"
    This reverts commit 7da92bae6928acca106e2a4f622b08cf9ab7e6f7.

--- parts/vendors/drive-chicago-footer.php
+++ parts/vendors/drive-chicago-footer.php
@@ -5,3 +5,10 @@
         })
     }
 </script>
+<script>
+    if(window.SECTIONS_FOR_DRIVE_CHICAGO){
+        document.addEventListener("DOMContentLoaded", function(){
+            window.DriveChicago && DriveChicago.inject(window.SECTIONS_FOR_DRIVE_CHICAGO)
+        })
+    }
+</script>
