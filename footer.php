cabc1d7236ac7d9ff4198e011149dd5af1fd7a24
Sean Hayes <seanchayes@gmail.com>
2016-08-05 10:26:04 -0500

    Incoming changes from preproduction svn commits

--- footer.php
+++ footer.php
@@ -11,6 +11,7 @@
         get_template_part( 'parts/vendors/google-survey-footer' );
         get_template_part( 'parts/vendors/yieldmo-footer' );
         get_template_part( 'parts/vendors/aggrego-chatter-footer' );
+        get_template_part( 'parts/vendors/drive-chicago-footer' );
     endif;
 ?>
 <?php get_template_part( 'parts/analytics/chartbeat-footer' ); ?>
---
+++ parts/vendors/drive-chicago-footer.php
@@ -0,0 +1,14 @@
+<script>
+    if(window.SECTIONS_FOR_DRIVE_CHICAGO){
+        document.addEventListener("DOMContentLoaded", function(){
+            window.DriveChicago && DriveChicago.inject(window.SECTIONS_FOR_DRIVE_CHICAGO)
+        })
+    }
+</script>
+<script>
+    if(window.SECTIONS_FOR_DRIVE_CHICAGO){
+        document.addEventListener("DOMContentLoaded", function(){
+            window.DriveChicago && DriveChicago.inject(window.SECTIONS_FOR_DRIVE_CHICAGO)
+        })
+    }
+</script>
