f13ae65d1b8a4a6d68bb44e76a39bd4b7cb9f01b
Sean Hayes <seanchayes@gmail.com>
2016-08-09 11:54:28 -0500

    Code compliance, escaping and formatting

--- parts/homepage/header.php
+++ parts/homepage/header.php
@@ -9,7 +9,7 @@
 						<li class="name">
 							<h1>
 								<a class="logo small-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-									<?php get_template_part( 'parts/images/company-logo'); ?>
+									<?php get_template_part( 'parts/images/company-logo' ); ?>
 								</a>
 							</h1>
 						</li>
@@ -22,25 +22,25 @@
 						<div class="top-section-ad right"></div>
 						<div class="logo-wrapper large-logo">
 							<div class="logo">
-								<img src="<?php echo get_template_directory_uri(); ?>/cst-white-logo.png" alt="CST LOGO">
-								<div class="masthead-date"><?php echo date( 'l, F j, Y' ) ?></div>
+								<img src="<?php echo esc_url( get_template_directory_uri() . '/cst-white-logo.png' ); ?>" alt='Chicago Sun-Times logo'>
+								<div class="masthead-date"><?php echo esc_html( date( 'l, F j, Y' ) ); ?></div>
 							</div>
 
 					</div>
 
-					<?php 
-						wp_nav_menu( array( 
-									'theme_location' => 'homepage-menu', 
-									'fallback_cb' => false,
-									'container' => false,
-									'depth' => 2,
-									'items_wrap' => '<ul id="%1$s" class="">%3$s</ul>',
-									'walker' => new GC_walker_nav_menu()
-								) 
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'homepage-menu',
+							'fallback_cb' => false,
+							'container' => false,
+							'depth' => 2,
+							'items_wrap' => '<ul id="%1$s" class="">%3$s</ul>',
+							'walker' => new GC_walker_nav_menu(),
+							)
 						);
 					?>
-				</section>
-			</nav>
-
+					</section>
+				</nav>
+			</div>
 		</div>
 	</header>
