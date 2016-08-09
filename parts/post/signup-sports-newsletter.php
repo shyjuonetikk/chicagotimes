ab9cc9d5aa187edb39fcd3de4dbd65132c8addb3
Michael Loring <mdloring@gmail.com>
2016-08-09 18:57:20 -0500

    insertion templates for politics/news and sports newsletter signups

---
+++ parts/post/signup-sports-newsletter.php
@@ -0,0 +1,9 @@
+<div class="large-12 columns entertainment-sign-up">
+    <h3><?php esc_html_e( 'Sign-Up for Chicago Sun-Times Sports Newsletter' ); ?></h3>
+    <span>
+        <?php esc_html_e( 'Receive Sports related Updates' ); ?>
+    </span>
+    <a href="<?php echo esc_url( 'https://r1.surveysandforms.com/062jcp97-06149p3a' ); ?>" target="_blank" class="button tiny danger">
+        <i class="fa fa-envelope"></i> <?php esc_html_e( 'Sign-Up' ); ?>
+    </a>
+</div>
