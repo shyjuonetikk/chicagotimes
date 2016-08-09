375fb42b90f74b6c6373c28ef7592b157c913cbe
Sean Hayes <seanchayes@gmail.com>
2016-08-09 12:31:45 -0500

    Article page search widget styling too, removing constraining div in header

--- parts/homepage/header.php
+++ parts/homepage/header.php
@@ -3,44 +3,42 @@
 	<!-- top logos -->
 	<header id="header">
 		<div class="contain-to-grid sticky">
-			<div class="cst-row">
-				<nav class="top-bar" data-topbar>
-					<ul class="title-area">
-						<li class="name">
-							<h1>
-								<a class="logo small-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-									<?php get_template_part( 'parts/images/company-logo' ); ?>
-								</a>
-							</h1>
-						</li>
-						<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-					</ul>
+			<nav class="top-bar" data-topbar>
+				<ul class="title-area">
+					<li class="name">
+						<h1>
+							<a class="logo small-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+								<?php get_template_part( 'parts/images/company-logo' ); ?>
+							</a>
+						</h1>
+					</li>
+					<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+				</ul>
 
 
-					<section class="top-bar-section">
-						<div class="top-section-ad left"></div>
-						<div class="top-section-ad right"></div>
-						<div class="logo-wrapper large-logo">
-							<div class="logo">
-								<img src="<?php echo esc_url( get_template_directory_uri() . '/cst-white-logo.png' ); ?>" alt='Chicago Sun-Times logo'>
-								<div class="masthead-date"><?php echo esc_html( date( 'l, F j, Y' ) ); ?></div>
-							</div>
+				<section class="top-bar-section">
+					<div class="top-section-ad left"></div>
+					<div class="top-section-ad right"></div>
+					<div class="logo-wrapper large-logo">
+						<div class="logo">
+							<img src="<?php echo esc_url( get_template_directory_uri() . '/cst-white-logo.png' ); ?>" alt='Chicago Sun-Times logo'>
+							<div class="masthead-date"><?php echo esc_html( date( 'l, F j, Y' ) ); ?></div>
+						</div>
 
-					</div>
+				</div>
 
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'homepage-menu',
-							'fallback_cb' => false,
-							'container' => false,
-							'depth' => 2,
-							'items_wrap' => '<ul id="%1$s" class="">%3$s</ul>',
-							'walker' => new GC_walker_nav_menu(),
-							)
-						);
-					?>
-					</section>
-				</nav>
-			</div>
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'homepage-menu',
+						'fallback_cb' => false,
+						'container' => false,
+						'depth' => 2,
+						'items_wrap' => '<ul id="%1$s" class="">%3$s</ul>',
+						'walker' => new GC_walker_nav_menu(),
+						)
+					);
+				?>
+				</section>
+			</nav>
 		</div>
 	</header>
