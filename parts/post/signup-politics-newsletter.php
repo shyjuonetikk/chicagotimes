8cd2c801b05683d2e671f4e38bda064d8d40c030
Michael Loring <mdloring@gmail.com>
2016-08-10 16:32:32 -0500

    change politics newsletter wording

--- parts/post/signup-politics-newsletter.php
+++ parts/post/signup-politics-newsletter.php
@@ -1,7 +1,7 @@
 <div class="large-12 columns entertainment-sign-up">
-    <h3><?php esc_html_e( 'Sign-Up for Chicago Sun-Times Politics Newsletter' ); ?></h3>
+    <h3><?php esc_html_e( 'Sign-Up for Chicago Sun-Times Politics & News Newsletter' ); ?></h3>
     <span>
-        <?php esc_html_e( 'Receive Political related Updates' ); ?>
+        <?php esc_html_e( 'Receive Political and News related Updates' ); ?>
     </span>
     <a href="<?php echo esc_url( 'https://r1.surveysandforms.com/062jcp97-2819pvaa' ); ?>" target="_blank" class="button tiny info">
         <i class="fa fa-envelope"></i> <?php esc_html_e( 'Sign-Up' ); ?>
