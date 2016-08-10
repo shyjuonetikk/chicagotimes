627c406c012f8567b6767fa69beced8c68018735
Michael Loring <mdloring@gmail.com>
2016-08-09 19:05:28 -0500

    update sports newsletter button color. only display 1 of the newsletter injection forms

--- parts/post/signup-sports-newsletter.php
+++ parts/post/signup-sports-newsletter.php
@@ -3,7 +3,7 @@
     <span>
         <?php esc_html_e( 'Receive Sports related Updates' ); ?>
     </span>
-    <a href="<?php echo esc_url( 'https://r1.surveysandforms.com/062jcp97-06149p3a' ); ?>" target="_blank" class="button tiny danger">
+    <a href="<?php echo esc_url( 'https://r1.surveysandforms.com/062jcp97-06149p3a' ); ?>" target="_blank" class="button tiny alert">
         <i class="fa fa-envelope"></i> <?php esc_html_e( 'Sign-Up' ); ?>
     </a>
 </div>
