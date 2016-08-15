c073f0fc4d4a8e4cdd6422aa15b7e315e0fd5a91
Sean Hayes <seanchayes@gmail.com>
2016-08-15 11:12:50 -0500

    Zedo ad unit inserts in first non wp-caption-text paragraph tag after the first, remove existing hardcoded zedo div, add javascript call to infinite-scroll handler

---
+++ assets/js/zedo-video.js
@@ -0,0 +1,53 @@
+window.CSTZedo = {
+
+  inject: function() {
+
+    postContent = jQuery('#main').find('.cst-active-scroll-post');
+    if ( ! postContent.length ) {
+      return;
+    }
+    var paragraphs = Array.prototype.slice.call(jQuery('.cst-active-scroll-post p'));
+    var paragraphsCount = paragraphs.length;
+    if ( paragraphsCount ) {
+      if( ! postContent.hasClass('zedo-inserted') ) {
+        if(paragraphsCount >= 3) {
+          zedoContentNode = this._insertHTMLTag();
+          jQuery('.cst-active-scroll-post .post-content').children('p').not('.wp-caption-text, :first').append(zedoContentNode);
+          this._insertZedoJS();
+          postContent.addClass('zedo-inserted');
+        }
+      }
+
+    }
+  },
+
+  /* Private methods */
+  _insertZedoJS: function(){
+    !function (a, n, e, t, r) {
+      tagsync = e;
+      var c = window[a];
+      if (tagsync) {
+        var d = document.createElement("script");
+        d.src = "http://3107.tm.zedo.com/v1/29252020-9011-4261-83dd-83503d457fb9/atm.js", d.async = !0;
+        var i = document.getElementById(n);
+        if (null == i || "undefined" == i)return;
+        i.parentNode.appendChild(d, i), d.onload = d.onreadystatechange = function () {
+          var a = new zTagManager(n);
+          a.initTagManager(n, c, this.aync, t, r)
+        }
+      } else document.write("<script src='http://3107.tm.zedo.com/v1/29252020-9011-4261-83dd-83503d457fb9/tm.js?data=" + a + "'><" + "/script>")
+    }("datalayer", "z578f1ef7-f0c5-4f8d-9f90-f7f7b7dc0206", true, 1, 1);
+  },
+
+  _insertHTMLTag: function(){
+
+    cst_div_div = jQuery('<div />');
+    cst_div_div.attr( 'id', 'z578f1ef7-f0c5-4f8d-9f90-f7f7b7dc0206');
+    cst_div_div.attr( 'style', 'display:none;' );
+
+    return cst_div_div;
+  }
+};
+document.addEventListener("DOMContentLoaded", function(){
+  window.CSTZedo && window.CSTZedo.inject()
+});
