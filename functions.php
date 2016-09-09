d49941d86b96cccaeb5a92dfe0befd8a029e3e77
Sean Hayes <seanchayes@gmail.com>
2016-09-09 14:43:49 -0500

    Restrictions for adops user role, removing menu items based on restriction, test functionality still works for sponsored sections

--- functions.php
+++ functions.php
@@ -215,7 +215,10 @@ class CST {
 		wpcom_vip_load_plugin( 'fieldmanager' );
 		wpcom_vip_load_plugin( 'pushup' );
 		wpcom_vip_load_plugin( 'wpcom-thumbnail-editor' );
-		wpcom_vip_load_plugin( 'zoninator' );
+		if ( ! current_user_can( 'adops' ) ) {
+			// Auto removes menu entry preventing adops role users from seeing it
+			wpcom_vip_load_plugin( 'zoninator' );
+		}
 		wpcom_vip_load_plugin( 'maintenance-mode' );
 		wpcom_vip_load_plugin( 'wpcom-legacy-redirector' );
 		if ( ! defined( 'WP_CLI' ) ) {
@@ -335,6 +338,14 @@ class CST {
 			add_action( 'above-homepage-headlines', array( CST_Elections::get_instance(), 'election_shortcode' ) );
 		}
 
+		add_action( 'init', function() {
+			// Add custom AdOps role
+			wpcom_vip_add_role( 'adops', 'Ad Ops', array(
+				'upload_files' => true,
+				'adops' => true,
+				'read' => true,
+			));
+		} );
 	}
 
 	/**
@@ -452,6 +463,38 @@ class CST {
             return array( 'cst_article', 'cst_gallery' );
         } );
 
+		add_filter( 'user_has_cap', array( $this, 'adops_cap_filter' ), 10, 3 );
+	}
+
+	/**
+	 * https://codex.wordpress.org/Plugin_API/Filter_Reference/user_has_cap
+	 *
+	 * Filter on the current_user_can() function.
+	 * Specifically for adops user restrictions to edit section
+	 * sponsorship options
+	 *
+	 * @param array $all_capabilities All the capabilities of the user
+	 * @param array $cap  [0] Required capability
+	 * @param array $args [0] Requested capability
+	 *                    [1] User ID
+	 *                    [2] Associated object ID
+	 *
+	 * @return mixed
+	 */
+	function adops_cap_filter( $all_capabilities, $cap, $args ) {
+		if ( 'edit_others_posts' !== $args[0] ) {
+			return $all_capabilities;
+		}
+
+		if ( ! $all_capabilities['adops'] ) {
+			return $all_capabilities;
+		}
+
+		$all_capabilities['manage_terms'] = true;
+		$all_capabilities['edit_others_posts'] = true;
+		$all_capabilities['upload_files'] = true;
+
+		return $all_capabilities;
 	}
 
 	/**
