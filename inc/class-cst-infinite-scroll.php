294a685840fc11b370575131d794804b800b3dd9
Sean Hayes <seanchayes@gmail.com>
2016-09-16 10:04:40 -0500

    Monster page, logic to include and exclude theme and ad components for this page, custom js, custom page template

--- inc/class-cst-infinite-scroll.php
+++ inc/class-cst-infinite-scroll.php
@@ -136,13 +136,14 @@ class CST_Infinite_Scroll {
 	 * Infinite scroll JavaScript hacks
 	 */
 	public function action_wp_enqueue_scripts() {
-
+		if ( is_page_template( 'page-monster.php' ) || is_front_page() ) {
+			return;
+		}
 		wp_enqueue_script( 'cst-infinite-scroll', get_template_directory_uri() . '/assets/js/infinite-scroll.js', array( 'chicagosuntimes', 'the-neverending-homepage', 'cst-ga-custom-actions' ), false, true );
 		wp_localize_script( 'cst-infinite-scroll', 'CSTInfiniteScrollData', array(
-			'infiniteSidebarEndpoint'         => esc_url_raw( home_url( 'infinite-sidebar/' ) ),
-			'readMoreLabel'                   => esc_html__( 'Read More', 'chicagosuntimes' ),
-			) );
-
+			'infiniteSidebarEndpoint' => esc_url_raw( home_url( 'infinite-sidebar/' ) ),
+			'readMoreLabel'           => esc_html__( 'Read More', 'chicagosuntimes' ),
+		) );
 	}
 
 	/**
