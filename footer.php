e05c7d97ad1a62a255c76a48ccc705fa7d9ebeba
Sean Hayes <seanchayes@gmail.com>
2016-09-19 14:29:23 -0500

    WIP for GumGum in image advertising

--- footer.php
+++ footer.php
@@ -4,13 +4,16 @@
 </div>
 
 <?php wp_footer(); ?>
-<?php 
+<?php
     if( is_singular() ) :
         get_template_part( 'parts/vendors/google-survey-footer' );
         get_template_part( 'parts/vendors/yieldmo-footer' );
         get_template_part( 'parts/vendors/aggrego-chatter-footer' );
+        get_template_part( 'parts/vendors/gum-gum-footer' );
     endif;
 ?>
 <?php get_template_part( 'parts/analytics/chartbeat-footer' ); ?>
 </body>
 </html>
+<script type="text/javascript">ggv2id='7d41bba7';</script>
+<script type="text/javascript" src="//g2.gumgum.com/javascripts/ggv2.js"></script>
---
+++ parts/vendors/gum-gum-footer.php
@@ -0,0 +1,2 @@
+<script type="text/javascript">ggv2id='7d41bba7';</script>
+<script type="text/javascript" src="//g2.gumgum.com/javascripts/ggv2.js"></script>
