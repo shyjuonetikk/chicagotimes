bdac9f6c7ea4698f555a6d8088446213adcc899b
Sean Hayes <seanchayes@gmail.com>
2016-09-20 10:14:30 -0500

    Adding footer to homepage, styling and positioning, placeholders for future footer menu, adding menus to register function

--- footer.php
+++ footer.php
@@ -13,25 +13,7 @@
 ?>
 </div>
 <?php get_template_part( 'parts/analytics/chartbeat-footer' ); ?>
-<footer>
-	<div class="row">
-		<div class="small-12 large-3 columns">
-			<p class="copyright"><?php echo sprintf( 'Copyright &copy; 2005 - %s Chicago Sun-Times', date( 'Y' ) ); ?></p>
-		</div>
-		<div class="small-12 large-9 columns">
-			<?php
-			wp_nav_menu( array(
-					'theme_location' => 'homepage-footer-menu',
-					'fallback_cb' => false,
-					'container' => false,
-					'depth' => 1,
-					'items_wrap' => '<ul id="%1$s" class="">%3$s</ul>',
-					'walker' => new GC_walker_nav_menu()
-				)
-			);
-			?>
-		</div>
-	</div>
-</footer>
+</div>
+<?php get_template_part( 'parts/footer-brand-navigation' ); ?>
 </body>
 </html>
--- parts/footer-brand-navigation.php
+++ parts/footer-brand-navigation.php
@@ -6,16 +6,52 @@
 		<p class="copyright"><?php echo sprintf( 'Copyright &copy; 2005 - %s Chicago Sun-Times', date( 'Y' ) ); ?></p>
 	</div>
 	<div class="small-12 medium-7 columns">
-		<ul>
-			<li><a href="/about-us">About us</a></li>
-			<li><a href="/contact-us">Contact us</a></li>
-			<li><a href="/about-our-ads">About our ads</a></li>
-		</ul>
-		<ul>
-			<li><a href="/privacy-policy">Privacy Policy</a></li>
-			<li><a href="/terms-of-use">Terms of use</a></li>
-			<li><a href="http://payments.suntimes.com" target="_blank">Order Back Issues</a>&nbsp;<i class="fa fa-external-link" aria-hidden="true"></i></li>
-		</ul>
+<?php if ( has_nav_menu( 'page-footer-1' ) ) {
+wp_nav_menu( array(
+	'theme_location' => 'page-footer-1',
+	'fallback_cb' => false,
+	'container' => false,
+	'depth' => 2,
+	'items_wrap' => '<ul id="%1$s" class="">%3$s</ul>',
+	)
+);
+} else { ?>
+	<ul>
+		<li><a href="/about-us">About us</a></li>
+		<li><a href="/contact-us">Contact us</a></li>
+	</ul>
+<?php }	?>
+
+<?php if ( has_nav_menu( 'page-footer-2' ) ) {
+wp_nav_menu( array(
+	'theme_location' => 'page-footer-2',
+	'fallback_cb' => false,
+	'container' => false,
+	'depth' => 2,
+	'items_wrap' => '<ul id="%1$s" class="">%3$s</ul>',
+	)
+);
+} else { ?>
+	<ul>
+		<li><a href="/about-our-ads">About our ads</a></li>
+		<li><a href="/privacy-policy">Privacy Policy</a></li>
+	</ul>
+<?php }	?>
+<?php if ( has_nav_menu( 'page-footer-3' ) ) {
+wp_nav_menu( array(
+	'theme_location' => 'page-footer-3',
+	'fallback_cb' => false,
+	'container' => false,
+	'depth' => 2,
+	'items_wrap' => '<ul id="%1$s" class="">%3$s</ul>',
+	)
+);
+} else { ?>
+	<ul>
+		<li><a href="/terms-of-use">Terms of use</a></li>
+		<li><a href="http://payments.suntimes.com" target="_blank">Order Back Issues</a>&nbsp;<i class="fa fa-external-link" aria-hidden="true"></i></li>
+	</ul>
+<?php }	?>
 	</div>
 	<div class="small-12 medium-2 columns">
 		<?php echo CST()->get_template_part( 'social-links'); ?>
