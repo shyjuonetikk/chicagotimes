4b9b351002bb1c4390982c25d4711f2317c778f8
Sean Hayes <seanchayes@gmail.com>
2016-11-07 09:20:28 -0600

    Unapproved for now so reverting:
    Revert "Newsletter signup improvements, newsletter function vs multiple files, related styling, removing old files"
    
    This reverts commit 2bb7fec95f94d0d85d35377951ce2feb3ff2a858.

---
+++ parts/post/signup-politics-newsletter.php
@@ -0,0 +1,9 @@
+<div class="large-12 columns entertainment-sign-up">
+	<h3><?php esc_html_e( 'Sign-Up for Chicago Sun-Times News & Politics Newsletter' ); ?></h3>
+	<span>
+        <?php esc_html_e( 'Receive News and Politics related Updates' ); ?>
+    </span>
+	<a href="<?php echo esc_url( 'https://r1.surveysandforms.com/062jcp97-2819pvaa' ); ?>" target="_blank" class="button tiny info">
+		<i class="fa fa-envelope"></i> <?php esc_html_e( 'Sign-Up' ); ?>
+	</a>
+</div>
