aad5e5f43585da36897fa129befdd9593ff25f05
Sean Hayes <seanchayes@gmail.com>
2016-11-07 11:18:44 -0600

    Revert "Newsletter changes approved now. Reverting this revert. Unapproved for now so reverting:"
    This reverts commit 4b9b351002bb1c4390982c25d4711f2317c778f8.

--- parts/post/signup-sports-newsletter.php
+++
@@ -1,9 +0,0 @@
-<div class="large-12 columns entertainment-sign-up">
-    <h3><?php esc_html_e( 'Sign-Up for Chicago Sun-Times Sports Newsletter' ); ?></h3>
-    <span>
-        <?php esc_html_e( 'Receive Sports related Updates' ); ?>
-    </span>
-    <a href="<?php echo esc_url( 'https://r1.surveysandforms.com/062jcp97-06149p3a' ); ?>" target="_blank" class="button tiny alert">
-        <i class="fa fa-envelope"></i> <?php esc_html_e( 'Sign-Up' ); ?>
-    </a>
-</div>
