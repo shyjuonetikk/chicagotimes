c2b74a1d3774853f6e0e93ede0bfff33c47d7cc5
Sean Hayes <seanchayes@gmail.com>
2016-12-01 08:58:17 -0600

    Adding in CST app logo to AMP banner

--- amp/header-bar.php
+++ amp/header-bar.php
@@ -7,8 +7,8 @@
 	<button on='tap:sidebar.open' class="cst-button">&#9776;</button>
 </div>
 <amp-app-banner layout="nodisplay" id="cst-app-banner">
-	<amp-img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/favicons/apple-touch-icon-57x57.png"
-			 width="57" height="57">
+	<amp-img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/cst-app-logo.svg"
+			 width="60" height="60">
 	</amp-img>
 	<h5>Chicago Sun-Times</h5>
 	<p>Experience a richer experience on our mobile app!</p>
