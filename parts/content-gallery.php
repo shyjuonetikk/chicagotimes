2e21f7492adf6c68fc57f9c2613c319c05e9c64a
Sean Hayes <seanchayes@gmail.com>
2016-12-29 15:42:45 -0600

    Markup changes and code formatting, layout for other content-types.php

--- parts/content-gallery.php
+++ parts/content-gallery.php
@@ -1,29 +1,29 @@
 <?php echo CST()->get_template_part( 'post/title', $vars ); ?>
 
-<?php if ( ! empty( $is_main_query ) ) : ?>
+<?php if ( ! empty( $is_main_query ) ) { ?>
 
-	<?php if ( is_singular() ) : ?>
-		<div class="post-lead-media post-content columns medium-11 medium-offset-1 end">
-			<?php echo do_shortcode( '[cst-content id="' . $obj->get_id() . '"]' ); ?>
-		</div>
-		<?php echo CST()->get_template_part( 'post/meta-byline', array( 'obj' => $obj ) ); ?>
-		<?php if ( $obj->get_content() ) : ?>
-		<div class="post-content columns medium-9 medium-offset-1 end">
-			<script>
-				window.SECTIONS_FOR_YIELD_MO = <?php echo wp_json_encode( CST_Frontend::$post_sections ); ?>;
-			</script>
-			<?php $obj->the_content(); ?>
-		</div>
-		<?php endif; ?>
-	<?php else : ?>
-		<?php
-		if ( $obj->get_excerpt() ) :
-			echo CST()->get_template_part( 'post/post-excerpt', array( 'obj' => $obj ) );
-		endif;
-		?>
-		<?php if ( $obj->get_featured_image_id() ) {
-			echo CST()->get_template_part( 'post/wire-featured-image', array( 'obj' => $obj ) );
-		} ?>
-	<?php endif; ?>
+<?php if ( is_singular() ) { ?>
+<div class="post-lead-media post-content columns small-12 end">
+	<?php echo do_shortcode( '[cst-content id="' . $obj->get_id() . '"]' ); ?>
+</div>
+<?php echo CST()->get_template_part( 'post/meta-byline', array( 'obj' => $obj ) ); ?>
+<?php if ( $obj->get_content() ) { ?>
+<div class="post-content columns medium-9 medium-offset-1 end">
+	<script>
+		window.SECTIONS_FOR_YIELD_MO = <?php echo wp_json_encode( CST_Frontend::$post_sections ); ?>;
+	</script>
+	<?php $obj->the_content(); ?>
+</div>
+<?php } ?>
+<?php } else { ?>
+<?php
+if ( $obj->get_excerpt() ) {
+	echo CST()->get_template_part( 'post/post-excerpt', array( 'obj' => $obj ) );
+}
+?>
+<?php if ( $obj->get_featured_image_id() ) {
+	echo CST()->get_template_part( 'post/wire-featured-image', array( 'obj' => $obj ) );
+} ?>
+	<?php } ?>
 
-<?php endif; ?>
+<?php } ?>
