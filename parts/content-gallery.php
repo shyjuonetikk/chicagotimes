eebb3c0a46595f18682e8ec4b67c0d3c6e0dd91f
Sean Hayes <seanchayes@gmail.com>
2016-12-30 15:02:24 -0600

    Correcting markup for gallery post type

--- parts/content-gallery.php
+++ parts/content-gallery.php
@@ -2,28 +2,28 @@
 
 <?php if ( ! empty( $is_main_query ) ) { ?>
 
-<?php if ( is_singular() ) { ?>
-<div class="post-lead-media post-content columns small-12 end">
-	<?php echo do_shortcode( '[cst-content id="' . $obj->get_id() . '"]' ); ?>
-</div>
-<?php echo CST()->get_template_part( 'post/meta-byline', array( 'obj' => $obj ) ); ?>
-<?php if ( $obj->get_content() ) { ?>
-<div class="post-content columns medium-9 medium-offset-1 end">
-	<script>
-		window.SECTIONS_FOR_YIELD_MO = <?php echo wp_json_encode( CST_Frontend::$post_sections ); ?>;
-	</script>
-	<?php $obj->the_content(); ?>
-</div>
-<?php } ?>
-<?php } else { ?>
-<?php
-if ( $obj->get_excerpt() ) {
-	echo CST()->get_template_part( 'post/post-excerpt', array( 'obj' => $obj ) );
-}
-?>
-<?php if ( $obj->get_featured_image_id() ) {
-	echo CST()->get_template_part( 'post/wire-featured-image', array( 'obj' => $obj ) );
-} ?>
+	<?php if ( is_singular() ) { ?>
+		<div class="post-lead-media post-content columns small-12 end">
+			<?php echo do_shortcode( '[cst-content id="' . $obj->get_id() . '"]' ); ?>
+		</div>
+		<?php echo CST()->get_template_part( 'post/meta-byline', array( 'obj' => $obj ) ); ?>
+		<?php if ( $obj->get_content() ) { ?>
+			<div class="post-content columns small-12 end">
+				<script>
+                  window.SECTIONS_FOR_YIELD_MO = <?php echo wp_json_encode( CST_Frontend::$post_sections ); ?>;
+				</script>
+				<?php $obj->the_content(); ?>
+			</div>
+		<?php } ?>
+	<?php } else { ?>
+		<?php
+		if ( $obj->get_excerpt() ) {
+			echo CST()->get_template_part( 'post/post-excerpt', array( 'obj' => $obj ) );
+		}
+		?>
+		<?php if ( $obj->get_featured_image_id() ) {
+			echo CST()->get_template_part( 'post/wire-featured-image', array( 'obj' => $obj ) );
+		} ?>
 	<?php } ?>
 
-<?php } ?>
+	<?php } ?>
