48b21308345a532c913862c2a900a827b3736c8a
Sean Hayes <seanchayes@gmail.com>
2017-01-03 11:20:51 -0600

    Adjusting second and further ads to pull from ad targeting atf leaderboard 2 on single pages in infinite scroll

--- content.php
+++ content.php
@@ -62,7 +62,7 @@
 			<?php
 			global $wp_query;
 			if ( is_singular() ) {
-				echo CST()->dfp_handler->dynamic_unit( get_the_ID(), 'div-gpt-placement-a', 'dfp-placement', 'article_lead_unit_mapping', 'atf leaderboard' );
+				echo CST()->dfp_handler->dynamic_unit( get_the_ID(), 'div-gpt-placement-a', 'dfp-placement', 'article_lead_unit_mapping', 'atf leaderboard 2' );
 			} else {
 				$every_two = $wp_query->current_post % 2;
 				if ( ( ! $every_two ) ) {
