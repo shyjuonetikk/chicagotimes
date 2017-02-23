2897e85356924bca98e31c995a475cab9c2a17b6
Sean Hayes <seanchayes@gmail.com>
2017-02-23 09:22:59 -0600

    Adding titling to Features archive page

--- functions.php
+++ functions.php
@@ -1549,7 +1549,7 @@ class CST {
 	 * Add the gallery backdrop to the footer
 	 */
 	public function action_wp_footer_gallery_backdrop() {
-		if ( is_404() ) {
+		if ( is_404() || is_post_type_archive( 'cst_feature' ) ) {
 			return;
 		}
 		echo $this->get_template_part( 'post/gallery-backdrop' );
