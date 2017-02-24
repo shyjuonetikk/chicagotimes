7f0ed3a8951188b92f21e9f1806dac82e0d4a373
Sean Hayes <seanchayes@gmail.com>
2017-02-24 11:31:20 -0600

    Adding class to taboola container, removing related width constraints.

--- content.php
+++ content.php
@@ -71,7 +71,7 @@ if ( $obj && is_singular( 'cst_article' ) ) {
 		</article>
 
 		<?php if ( is_singular( 'cst_article' ) ) { ?>
-		<section class="taboola-container">
+		<section class="taboola-container medium-11 medium-offset-1 columns">
 		<?php get_template_part( 'parts/taboola/taboola-container' ); ?>
 		</section>
 		<?php } ?>
