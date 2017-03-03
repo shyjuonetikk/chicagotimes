9738b659bfa3b12e5e10394eb8b4f533b18b65c1
Sean Hayes <seanchayes@gmail.com>
2017-03-03 15:18:55 -0600

    Removing cst-gallery check in content-article which is for er articles

--- content.php
+++ content.php
@@ -9,7 +9,7 @@ if ( $obj && is_singular( 'cst_article' ) ) {
 }
 ?>
 <?php if ( ! is_sticky() ) : ?>
-	<?php if ( is_singular( array( 'cst_article', 'cst_gallery' ) ) ) : ?>
+	<?php if ( is_singular( array( 'cst_article' ) ) ) : ?>
 		<div class="row post-row">
 		<?php endif; ?>
 
@@ -28,7 +28,7 @@ if ( $obj && is_singular( 'cst_article' ) ) {
 			<?php if ( $sponsored ) { ?>
 				<div class="sponsored-treatment">
 			<?php } ?>
-			<?php if ( is_singular( array( 'cst_article', 'cst_gallery' ) ) ) : ?>
+			<?php if ( is_singular( array( 'cst_article' ) ) ) : ?>
 
 				<?php if ( 'cst_embed' !== $obj->get_post_type() || 'twitter' !== $obj->get_embed_type() ) : ?>
 					<div class="post-meta post-meta-social show-for-medium-up">
@@ -39,17 +39,15 @@ if ( $obj && is_singular( 'cst_article' ) ) {
 			<?php endif; ?>
 
 			<?php
-			if ( is_singular( array( 'cst_article', 'cst_gallery' ) ) || is_tax() ) {
-				echo CST()->get_template_part( 'post/meta-top', array( 'obj' => $obj, 'is_main_query' => true ) );
-			}
+			echo CST()->get_template_part( 'post/meta-top', array( 'obj' => $obj, 'is_main_query' => true ) );
 			echo CST()->get_template_part( 'content-' . str_replace( 'cst_', '', get_post_type() ), array( 'obj' => $obj, 'is_main_query' => true ) );
 			if ( $sponsored ) { ?>
 				</div>
 			<?php }
-			if ( is_tax() || is_singular( array( 'cst_article', 'cst_gallery' ) ) ) {
+			if ( is_tax() || is_singular( array( 'cst_article' ) ) ) {
 				echo CST()->get_template_part( 'post/meta-bottom', array( 'obj' => $obj, 'is_main_query' => true ) );
 			}
-			if ( is_singular( array( 'cst_article', 'cst_gallery' ) ) ) {
+			if ( is_singular( array( 'cst_article' ) ) ) {
 				echo CST()->get_template_part( 'post/post-recommendations-chartbeat', array( 'obj' => $obj ) );
 				CST()->frontend->inject_headlines_network_markup( $obj );
 			} ?>
@@ -63,7 +61,7 @@ if ( $obj && is_singular( 'cst_article' ) ) {
 
 		<?php CST()->frontend->content_ad_injection( $paged ); ?>
 
-		<?php if ( is_singular( array( 'cst_article', 'cst_gallery' ) ) ) : ?>
+		<?php if ( is_singular( array( 'cst_article' ) ) ) : ?>
 		</div>
 	<?php endif; ?>
 <?php endif; ?>
