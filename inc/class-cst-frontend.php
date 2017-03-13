7091eae72a03f7b093658488e773439cf5dbcf4e
Sean Hayes <seanchayes@gmail.com>
2017-03-13 09:34:44 -0500

    Escaping missed from previous commit

--- inc/class-cst-frontend.php
+++ inc/class-cst-frontend.php
@@ -1339,7 +1339,7 @@ class CST_Frontend {
 	*/
 	function inject_send_to_news_video_player( $slug, $id ) {
 		$template   = '<div class="video-injection"><div class="s2nPlayer k-%1$s %2$s" data-type="float"></div><script type="text/javascript" src="http://embed.sendtonews.com/player3/embedcode.js?fk=%1$s&cid=4661&offsetx=0&offsety=50&floatwidth=300&floatposition=top-left" data-type="s2nScript"></script></div>';
-		$markup     = sprintf( $template, $this->send_to_news_embeds[ $slug ], $this->post->ID );
+		$markup     = sprintf( $template, esc_attr( $this->send_to_news_embeds[ $slug ] ), esc_attr( $this->post->ID ) );
 		echo $markup;
 
 	}
--- inc/objects/class-article.php
+++ inc/objects/class-article.php
@@ -47,7 +47,7 @@ class Article extends Post {
 		if ( $media_type = $this->get_fm_field( 'cst_production', 'featured_media', 'featured_video' ) ) {
 			if ( array_key_exists( $media_type, $this->send_to_news_embeds ) ) {
 				$template   = '<div class="video-injection"><div class="s2nPlayer k-%1$s %2$s" data-type="float"></div><script type="text/javascript" src="http://embed.sendtonews.com/player3/embedcode.js?fk=%1$s&cid=4661&offsetx=0&offsety=50&floatwidth=300&floatposition=top-left" data-type="s2nScript"></script></div>';
-				$markup     = sprintf( $template, $this->send_to_news_embeds[ $media_type ], $this->post->ID );
+				$markup     = sprintf( $template, esc_attr( $this->send_to_news_embeds[ $media_type ] ), esc_attr( $this->post->ID ) );
 				return $markup;
 			} else {
 				return '';
