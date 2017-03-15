cef190fe807e6212266fcf1becec415ca8de9112
Sean Hayes <seanchayes@gmail.com>
2017-03-15 15:53:17 -0500

    Correction to feed definitions, check featured image, attached media credits for licensed content and prevent inclusion in partner feed

--- inc/class-cst-custom-feed.php
+++ inc/class-cst-custom-feed.php
@@ -107,6 +107,63 @@ class CST_Feeds {
 	}
 
 	/**
+	 * @param \CST\Objects\Article|\CST\Objects\Gallery $obj
+	 *
+	 * @return bool
+	 *
+	 * Deny publishing of content with licensed images in a partner feed
+	 */
+	private function contains_licensed_image( $obj ) {
+
+		if ( ! $obj ) {
+			// cannot deny or confirm
+			return false;
+		}
+
+		// get and check featured image - easiest
+		$featured_image_id = $obj->get_featured_image_id();
+		if ( $featured_image_id && $attachment = \CST\Objects\Attachment::get_by_post_id( $featured_image_id ) ) {
+			if ( $this->check_image_credit( $attachment ) ) {
+				return true;
+			}
+		}
+		// get and check attached images - easy
+		$attached_to_content = get_attached_media( 'image/*', $obj->get_id() );
+		if ( is_array( $attached_to_content ) && ! empty( $attached_to_content ) ) {
+			foreach ( $attached_to_content as $item ) {
+				$attachment = \CST\Objects\Attachment::get_by_post_id( $item->ID );
+				if ( $this->check_image_credit( $attachment ) ) {
+					return true;
+				}
+			}
+		}
+		// get and check inline images - if necessary hard
+		return false;
+	}
+
+	/**
+	 * @param $attachment
+	 *
+	 * @return bool
+	 * Establish if the content (an attachment) is from a licensed partner
+	 */
+	private function check_image_credit( $attachment ) {
+		$image_credit = $attachment->get_credit();
+		if ( '' !== $image_credit ) {
+			$licensed_photo_sources = array(
+				'AP Photo',
+				'Getty Images',
+			);
+			foreach ( $licensed_photo_sources as $licensed_photo_source ) {
+				if ( false !== strpos( $licensed_photo_source, $image_credit ) ) {
+					// License source found somewhere in credit meta
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	/**
 	 * @param $obj
 	 *
 	 * @return bool
@@ -118,7 +175,13 @@ class CST_Feeds {
 		$publish_me = false;
 		if ( $this->is_publishing_partner() ) {
 			if ( ! $this->is_syndicated_content( $obj ) ) {
-				$publish_me = true;
+				if ( $this->contains_licensed_image( $obj ) ) {
+					return false;
+				}
+				return true;
+			}
+			if ( $this->contains_licensed_image( $obj ) ) {
+				return false;
 			}
 		} else {
 			$publish_me = true;
