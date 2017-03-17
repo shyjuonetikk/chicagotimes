8cd24bae2d5a521e98a7a2d174f94af49adf56b9
Sean Hayes <seanchayes@gmail.com>
2017-03-17 15:23:53 -0500

    Markup handling for homepage as well as right rail for the CB widget

--- inc/widgets/class-cst-cb-trending-widget.php
+++ inc/widgets/class-cst-cb-trending-widget.php
@@ -15,16 +15,20 @@ class CST_CB_Trending_Widget extends WP_Widget {
 	}
 
 	public function widget( $args, $instance ) {
-		echo wp_kses_post( $args['before_widget'] );
-
-		echo wp_kses_post( $args['before_title'] . 'Currently Trending' . $args['after_title'] );
+		if ( is_front_page() ) {
+			echo wp_kses_post( '<div class="small-12 content-wrapper"><hr class="before"><h2 class="section-title"><span>Currently trending:</span></h2><hr>' );
+		} else {
+			echo wp_kses_post( $args['before_widget'] );
+			echo wp_kses_post( $args['before_title'] . 'Currently Trending' . $args['after_title'] );
+		}
 		?>
-		<div class="small-12">
-			<div id="root"><p>Loading...</p></div>
-			<script type="text/javascript" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/static/js/main.6c13c9a2.js"></script>
-		</div>
-		<?php
-		echo wp_kses_post( $args['after_widget'] );
+		<div id="root"></div>
+		<script type="text/javascript" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/static/js/main.6c13c9a2.js"></script>
+		<?php if ( is_front_page() ) {
+			echo wp_kses_post( '</div>' );
+		}else if ( is_singular() || is_tax() || is_post_type_archive() ) {
+			echo wp_kses_post( $args['after_widget'] );
+		}
 	}
 
 	public function form( $instance ) {
