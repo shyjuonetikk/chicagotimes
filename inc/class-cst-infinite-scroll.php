1439f773896e2ec5ce2fd5a1efab27830ad86bd9
Sean Hayes <seanchayes@gmail.com>
2017-03-22 10:45:54 -0500

    Checking we have a video setting to process in the upcoming article during infinite scroll

--- inc/class-cst-infinite-scroll.php
+++ inc/class-cst-infinite-scroll.php
@@ -162,12 +162,15 @@ class CST_Infinite_Scroll {
 		) );
 		if ( is_singular( array( 'cst_article' ) ) ) {
 			$obj = \CST\Objects\Post::get_by_post_id( get_the_ID() );
+			$media_type = $obj->get_featured_media_type();
+			if ( 'video' === $media_type ) {
 			$embed_code = $obj->get_featured_video_script();
 			wp_enqueue_script( 'send-to-news' . '-' . esc_attr( $embed_code . '-' . esc_attr( $obj->get_id() ) ),
 				esc_url(
 					sprintf(
 						'http://embed.sendtonews.com/player3/embedcode.js?fk=%1$s&cid=4661&offsetx=0&offsety=50&floatwidth=300&floatposition=top-left', esc_attr( $embed_code ) )
 				), array( 'chicagosuntimes' ), null, true );
+			}
 		}
 	}
 
