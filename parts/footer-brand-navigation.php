bf596a8ea26b3c752ac5bc806c632cd55c75b933
Sean Hayes <seanchayes@gmail.com>
2017-03-27 10:57:24 -0500

    Removing responsive setting from subscribe iframe/modal, setting width on subscribe button in masthead

--- parts/footer-brand-navigation.php
+++ parts/footer-brand-navigation.php
@@ -100,7 +100,7 @@ if ( has_nav_menu( 'page-footer-4' ) ) {
 </footer>
 <?php } ?>
 <div id="subscribe-modal" class="reveal-modal xlarge" data-reveal aria-labelledby="Subscribe to Chicago Sun-Times" aria-hidden="true" role="dialog">
-	<iframe src="http://wssp.suntimes.com/subscribe/" frameborder="0" class="cst-responsive" width="100%" height="700" allowfullscreen></iframe>
+	<iframe src="http://wssp.suntimes.com/subscribe/" frameborder="0" width="100%" height="700" allowfullscreen></iframe>
 	<a class="close-reveal-modal" aria-label="Close">&#215;</a>
 </div>
 <?php
