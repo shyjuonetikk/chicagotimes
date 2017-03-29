53902363caf9a04df374e7c049d2b739f02cac0a
Sean Hayes <seanchayes@gmail.com>
2017-03-29 15:25:43 -0500

    Correcting ad targeting value calls, adding ads to author pages

--- parts/homepage/right-sidebar.php
+++ parts/homepage/right-sidebar.php
@@ -1,6 +1,6 @@
 <?php if ( dynamic_sidebar( 'homepage_sidebar' ) ) { } ?>
 <div class="large-12 small-12 columns">
 	<div class="row">
-		<?php echo CST()->dfp_handler->dynamic_unit( 1, 'div-gpt-rr-cube', 'dfp dfp-cube' ); ?>
+		<?php echo CST()->dfp_handler->dynamic_unit( 1, 'div-gpt-rr-cube', 'dfp dfp-cube', 'hp_cube_mapping', 'rr cube 1' ); ?>
 	</div>
 </div>
