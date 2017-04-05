da3cb5533532996636fff665a67087f1c5111398
Sean Hayes <seanchayes@gmail.com>
2017-04-05 08:30:44 -0500

    Adding in nav positions for pre-population

--- functions.php
+++ functions.php
@@ -837,6 +837,8 @@ class CST {
 		register_nav_menus(
 			array(
 				'homepage-menu'          => esc_html__( 'Homepage', 'chicagosuntimes' ),
+				'homepage-masthead'      => esc_html__( 'Homepage Masthead', 'chicagosuntimes' ),
+				'homepage-itn'           => esc_html__( 'Homepage Trending', 'chicagosuntimes' ),
 				'homepage-footer-menu'   => esc_html__( 'Homepage Footer', 'chicagosuntimes' ),
 				'news-menu'              => esc_html__( 'News', 'chicagosuntimes' ),
 				'news-trending'          => esc_html__( 'News Trending', 'chicagosuntimes' ),
@@ -857,6 +859,7 @@ class CST {
 				'page-footer-1'          => esc_html__( 'Page Footer 1', 'chicagosuntimes' ),
 				'page-footer-2'          => esc_html__( 'Page Footer 2', 'chicagosuntimes' ),
 				'page-footer-3'          => esc_html__( 'Page Footer 3', 'chicagosuntimes' ),
+				'page-footer-4'          => esc_html__( 'Page Footer 4', 'chicagosuntimes' ),
 				'election-page'          => esc_html__( 'Election Page', 'chicagosuntimes' ),
 			)
 		);
