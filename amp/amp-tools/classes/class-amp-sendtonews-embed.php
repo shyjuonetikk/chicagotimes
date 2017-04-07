a90d0e832423c786bb1228c3cbceec890b7c9e44
Sean Hayes <seanchayes@gmail.com>
2017-04-07 14:34:14 -0500

    WIP for SendToNews refactor - now an embed, trying to setup iframe correctly

--- amp/amp-tools/classes/class-amp-sendtonews-embed.php
+++ amp/amp-tools/classes/class-amp-sendtonews-embed.php
@@ -2,12 +2,12 @@
 /**
  * Class CST_AMP_Send_To_News_Embed
  *
- * Basic sanitizer functionality for Send To News video embeds
+ * Basic embed functionality for Send To News video embeds
  * Includes legacy video player for AMP and related embed code mapping
  *
  */
 class CST_AMP_Send_To_News_Embed extends AMP_Base_Embed_Handler {
-	private static $script_slug = 'amp-frame';
+	private static $script_slug = 'amp-iframe';
 	private static $script_src = 'https://cdn.ampproject.org/v0/amp-iframe-0.1.js';
 	private $send_to_news_embeds_map = array(
 		'xXrmaE8c'=> 'uqWfqG2Y',
@@ -34,43 +34,42 @@ class CST_AMP_Send_To_News_Embed extends AMP_Base_Embed_Handler {
 	 *
 	 * Find player id from html / dom being processed
 	 */
-	private function cst_build_send_to_news_element() {
-		$xp = new DOMXPath( $this->dom );
-		$player_div = $xp->query('//span[contains(concat(" ", normalize-space(@class), " "), " s2nlegacy ")]');
-		if ( $player_div ) {
-			$player_present = $player_div->item(0);
-			if ( null !== $player_present ) {
-				if ( $player_div->item( 0 )->hasAttribute( 'data-video-id' ) ) {
-					$player_id = $player_div->item( 0 )->getAttribute( 'data-video-id' );
-					$this->did_convert_elements = true;
-					return $this->render( $this->send_to_news_embeds_map[ $player_id ] );
-				}
-			}
+	public function cst_build_send_to_news_element( $content ) {
+
+		$obj = new CST\Objects\Article( get_the_ID() );
+		$media_type = $obj->get_featured_media_type();
+		if ( 'video' === $media_type ) {
+			$player_id = $obj->get_featured_video_script();
+			$this->did_convert_elements = true;
+			return $content . $this->render( array( 'player_id' => $player_id ) );
 		} else {
-			return false;
+			return $content;
 		}
+
 	}
 
 	public function render( $args ) {
+
 		$args = wp_parse_args( $args, array(
-			'images' => false,
+			'player_id' => false,
 		) );
 
-		if ( empty( $args['images'] ) ) {
+		if ( empty( $args['player_id'] ) ) {
 			return '';
 		}
 
-		return AMP_HTML_Utils::build_tag(
-			'amp-iframe',
+		$b = AMP_HTML_Utils::build_tag(
+			'iframe',
 			array(
-				'width'  => $this->args['width'],
-				'height' => $this->args['height'],
-				'type'   => 'slides',
+				'href'   => esc_url( 'http://embed.sendtonews.com/player2/embedcode.php?type=full&fk=' . $args['player_id'] . '&cid=4661' ),
+				'height' => '400',
+				'frameborder' => '0',
+				'scrolling' => 'no',
+				'sandbox' => 'allow-scripts allow-same-origin allow-popups',
 				'layout' => 'responsive',
-				'class'  => 'cst-amp-carousel',
-			),
-			implode( PHP_EOL, $images )
+			)
 		);
+		return $b;
 	}
 		/**
 	 * @return array
