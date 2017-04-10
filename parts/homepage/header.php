a630f9d9536c15f21d510d08e4c37fc321231939
Sean Hayes <seanchayes@gmail.com>
2017-04-10 15:21:30 -0500

    Adding in superleaderboard 2 onto homepage

--- parts/homepage/header.php
+++ parts/homepage/header.php
@@ -2,3 +2,9 @@
 	get_template_part( 'parts/header-brand-navigation' );
 } ?>
 <div class="homepage_wrapper">
+<?php
+if ( is_home() || is_front_page() ) {
+	echo wp_kses( CST()->dfp_handler->dynamic_unit( 2, 'div-gpt-super-leaderboard', 'dfp dfp-super-leaderboard dfp-centered', 'super_leaderboard_mapping', 'Super leaderboard 2 970x90' ),
+		CST()->dfp_kses
+	);
+}
