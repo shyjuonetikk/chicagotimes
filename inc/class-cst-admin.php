5e71a9a5d5fcf00928091616193ddd50f3841793
Sean Hayes <seanchayes@gmail.com>
2017-04-12 14:26:23 -0500

    Correcting parameters for cache_invalidate filter

--- inc/class-cst-admin.php
+++ inc/class-cst-admin.php
@@ -83,7 +83,7 @@ class CST_Admin {
 		});
 		add_action( 'fm_term_cst_section', array( $this, 'section_sponsorship_fields' ) );
 		add_action( 'wp_update_nav_menu_item', array( $this, 'amp_nav_invalidate_cache' ) );
-		add_action( 'wp_update_nav_menu_item', array( $this, 'section_nav_invalidate' ) );
+		add_action( 'wp_update_nav_menu_item', array( $this, 'section_nav_invalidate' ), 10, 3 );
 		add_action( 'post_submitbox_misc_actions', array( $this, 'trigger_notification_button' ) );
 	}
 
@@ -1283,10 +1283,11 @@ class CST_Admin {
 	 *
 	 * @param $menu_id
 	 * @param $menu_item_id
+	 * @param $args
 	 *
 	 * See class-cst-frontend.php -> get_sections_nav()
 	 */
-	public function section_nav_invalidate( $menu_id, $menu_item_id ) {
+	public function section_nav_invalidate( $menu_id, $menu_item_id, $args ) {
 		wpcom_vip_cache_delete( 'cst_homepage', 'default' );
 		wpcom_vip_cache_delete( 'cst_section-front', 'default' );
 		wpcom_vip_cache_delete( 'cst_homepage-itn', 'default' );
