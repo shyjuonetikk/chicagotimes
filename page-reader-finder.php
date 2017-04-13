24eb06a51c884bf09e3780e13954382ea03d71a1
Sean Hayes <seanchayes@gmail.com>
2017-04-13 13:45:08 -0500

    Markup changes to give scroll finger touch room on mobile

--- page-reader-finder.php
+++ page-reader-finder.php
@@ -7,8 +7,10 @@
 
 <section>
 	<div class="row page-content">
-	<h1>Find a Reader near you</h1>
-	<iframe src="https://www.google.com/maps/d/embed?mid=1qpwJ1K2nm2-gUK2qUgyo-i839P8" width="100%" height="800px" ></iframe>
+		<div class="small-12 columns">
+			<h1>Find a Reader near you</h1>
+			<iframe src="https://www.google.com/maps/d/embed?mid=1qpwJ1K2nm2-gUK2qUgyo-i839P8" width="100%" height="800px" ></iframe>
+		</div>
 	</div>
 </section>
 
