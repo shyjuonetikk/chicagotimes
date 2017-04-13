cb8fd875e45aff14fc2bbd13af783188eb8bd377
Randal Svea <rsvea@suntimes.com>
2017-04-13 11:13:05 -0500

    demo mymaps version for CST, Reader, and both

--- page-paper-finder-both.php
+++ page-paper-finder-both.php
@@ -1,6 +1,6 @@
 <?php
   /* 
-   * Template Name: Paper Finder
+   * Template Name: Paper Finder - CST-Reader
    */
 ?>
 <?php get_header(); ?>
@@ -9,7 +9,7 @@
 	<div class="row page-content">
 	<h1>Find a newspaper near you</h1>
     <p><em>Disclaimer: Not all locations carry the paper seven days a week.</em></p>
-	<iframe src="https://www.google.com/maps/d/embed?mid=14D1eo2aTdQAq776ZDxcdkSkLZNg" width="100%" height="800px" ></iframe>
+	<iframe src="https://www.google.com/maps/d/embed?mid=1XTtYzvv1SA01qmLxXv_H-9yLZ64" width="100%" height="800px" ></iframe>
 	</div>
 </section>
 
