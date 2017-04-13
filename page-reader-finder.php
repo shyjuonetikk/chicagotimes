a3c34effad027a9e1dd15d26296f791886529548
Sean Hayes <seanchayes@gmail.com>
2017-04-13 13:41:57 -0500

    Paper finder templates and styling

---
+++ page-reader-finder.php
@@ -0,0 +1,15 @@
+<?php
+  /* 
+   * Template Name: Reader Finder
+   */
+?>
+<?php get_header(); ?>
+
+<section>
+	<div class="row page-content">
+	<h1>Find a Reader near you</h1>
+	<iframe src="https://www.google.com/maps/d/embed?mid=1qpwJ1K2nm2-gUK2qUgyo-i839P8" width="100%" height="800px" ></iframe>
+	</div>
+</section>
+
+<?php get_footer();
