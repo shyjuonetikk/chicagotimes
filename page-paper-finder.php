9026a627fd70afd322f89d70c32cabcb2986c24e
Sean Hayes <seanchayes@gmail.com>
2017-04-14 11:56:26 -0500

    Testing Paper Finder using newly supplied code

--- page-paper-finder.php
+++ page-paper-finder.php
@@ -1,33 +1,18 @@
 <?php
-  /*
-   * Template Name: Paper Finder
-   */
+/*
+ * Template Name: Paper Finder
+ */
 ?>
-<?php get_header( 'page' ); ?>
-	<div class="row page-content">
-		<div class="small-12 columns">
-			<section id="subscribe">
-				<p>Choose your publication:</p>
-				<div class="cst-checkbox">
-					<input type="checkbox" checked="checked" name="publication" id="pub1" value="5300">
-					<label>
-						<img style="display:block; margin-left: 50px; margin-top: -25px;width:120px;" src="http://wssp.suntimes.com/wp-content/themes/market-cst/map/cst-masthead.jpg" alt="Chicago Sun-Times">
-						<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images'); ?>/cst-marker.jpg" style="margin-top:-25px;margin-left:15px;float:left;"/>
-					</label>
-				</div>
-				<div class="reader-checkbox">
-					<input type="checkbox" name="publication" id="pub2" value="5400">
-					<label>
-						<img style="display:block; margin-left: 50px; margin-top: -30px;" src="http://live-market-cst.pantheon.io/wp-content/uploads/2014/12/reader.png" alt="Reader">
-						<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images'); ?>/reader-marker.jpg" style="margin-top:-30px;margin-left:15px;float:left;"/>
-					</label>
-				</div>
+<?php get_header(); ?>
 
-				<div style="clear:both;"></div>
-				<small><em>Disclaimer: Not all locations carry the paper seven days a week.</em></small>
-				<input id="pac-input" class="controls" type="text" placeholder="Search Box"/>
-				<div id="map"></div>
-			</section>
+	<section>
+		<div class="row page-content">
+			<div class="small-12 columns">
+				<h1>Find a newspaper near you</h1>
+				<p><em>Disclaimer: Not all locations carry the paper seven days a week.</em></p>
+				<iframe src="https://www.google.com/maps/d/embed?mid=14D1eo2aTdQAq776ZDxcdkSkLZNg" width="100%" height="800px" ></iframe>
+			</div>
 		</div>
-	</div>
-<?php get_footer(); ?>
+	</section>
+
+<?php get_footer( );
