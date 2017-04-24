1a23e93586e642b3639edad17d90927222922c70
Randal Svea <rsvea@suntimes.com>
2017-04-24 12:25:21 -0500

    add apstag

--- assets/js/vendor/apstag-header.js
+++ assets/js/vendor/apstag-header.js
@@ -14,6 +14,7 @@ googletag.pubads().enableSingleRequest();
 googletag.enableServices();
 });
 
+//
 !function(a9,a,p,s,t,A,g){if(a[a9])return;function q(c,r){a[a9]._Q.push([c,r])}a[a9]={init:function(){q("i",arguments)},fetchBids:function()
 {q("f",arguments)},setDisplayBids:function(){},_Q:[]};A=p.createElement(s);A.async=!0;A.src=t;g=p.getElementsByTagName(s)[0];g.parentNode.insertBefore( A,g)}("apstag",window,document,"script","//c.amazon-adsystem.com/aax2/apstag.js");
 // initialize apstag and have apstag set bids on the googletag slots when they are returned to the page
