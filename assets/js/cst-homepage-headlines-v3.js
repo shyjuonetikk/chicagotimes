c1edbc32b0b0e642a69fdcaae9d91543a6ef85f8
Sean Hayes <seanchayes@gmail.com>
2017-04-26 13:52:02 -0500

    Updated Widget v3 for homepage headlines

---
+++ assets/js/cst-homepage-headlines-v3.js
@@ -0,0 +1,94 @@
+/* global CSTCategoryHeadlinesData */
+(function( $ ){
+
+    var CSTHomepageHeadlinesWidget = {
+
+        init: function() {
+
+            this.bindEvents();
+
+        },
+
+        bindEvents: function() {
+
+            $( document ).on( 'widget-updated widget-added', $.proxy( function( event, widget ){
+                var el = widget.find(".cst-homepage-headlines-one");
+                if ( el.length ) {
+                    this.loadSelect2( el );
+                }
+            }, this ) );
+
+            $(".cst-homepage-headlines-one").each( $.proxy( function( key, el ){
+                this.loadSelect2( $( el ) );
+            }, this ) );
+
+            $( document ).on( 'widget-updated widget-added', $.proxy( function( event, widget ){
+                var el = widget.find(".cst-homepage-headlines-two");
+                if ( el.length ) {
+                    this.loadSelect2( el );
+                }
+            }, this ) );
+
+            $(".cst-homepage-headlines-two").each( $.proxy( function( key, el ){
+                this.loadSelect2( $( el ) );
+            }, this ) );
+
+            $( document ).on( 'widget-updated widget-added', $.proxy( function( event, widget ){
+                var el = widget.find(".cst-homepage-headlines-three");
+                if ( el.length ) {
+                    this.loadSelect2( el );
+                }
+            }, this ) );
+
+            $(".cst-homepage-headlines-three").each( $.proxy( function( key, el ){
+                this.loadSelect2( $( el ) );
+            }, this ) );
+
+            $( document ).on( 'widget-updated widget-added', $.proxy( function( event, widget ){
+                var el = widget.find(".cst-homepage-headlines-four");
+                if ( el.length ) {
+                    this.loadSelect2( el );
+                }
+            }, this ) );
+
+        },
+
+        /**
+         * Load select2 for the element
+         */
+        loadSelect2: function( el ) {
+
+            el.select2({
+                placeholder: CSTCategoryHeadlinesData.placeholder_text,
+                minimumInputLength: 0,
+                allowClear: true,
+                ajax: {
+                    quietMillis: 150,
+                    url: ajaxurl,
+                    dataType: 'json',
+                    data: function ( term ) {
+                        return {
+                            action: 'cst_homepage_headlines_get_posts',
+                            nonce: CSTCategoryHeadlinesData.nonce,
+                            searchTerm: term
+                        };
+                    },
+                    results: function( data ) {
+                        return { results: data };
+                    }
+                },
+                initSelection: function( el, callback ) {
+                    callback( { id: el.val(), text: el.data('story-title' ) } );
+                }
+            });
+
+        }
+    };
+
+    $(document).ready(function(){
+
+        CSTHomepageHeadlinesWidget.init();
+
+    });
+
+}( jQuery ) );
