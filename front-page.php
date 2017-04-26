657b6dbf9390ded7ed391f0bc48127bfc196645e
Sean Hayes <seanchayes@gmail.com>
2017-04-26 13:00:58 -0500

    Adjusting style / sass imports, setting homepage row width

--- front-page.php
+++ front-page.php
@@ -1,58 +1,57 @@
 <?php get_header(); ?>
-<div class="homepage-content">
-	<div class="row">
-		<div class="large-12 content-wrapper">
-			<div class="large-12 columns dfp-mobile-leaderboard show-for-small-only">
-				<?php get_template_part( 'parts/dfp/homepage/dfp-mobile-leaderboard' ); ?>
+	<div class="homepage-content">
+		<div class="row">
+			<div class="large-12 content-wrapper">
+				<div class="large-12 columns dfp-mobile-leaderboard show-for-small-only">
+					<?php get_template_part( 'parts/dfp/homepage/dfp-mobile-leaderboard' ); ?>
+				</div>
 			</div>
 		</div>
-	</div>
-	<?php
-	do_action( 'above-homepage-headlines' );
-	if ( is_active_sidebar( 'homepage_headlines' ) ) :
-		dynamic_sidebar( 'homepage_headlines' );
-	endif;
-	?>
-</div>
-<div class="large-12 dfp-atf-leaderboard">
-	<?php echo wp_kses( CST()->dfp_handler->unit( 3, 'div-gpt-super-leaderboard', 'dfp dfp-super-leaderboard dfp-centered' ),
-		CST()->dfp_kses
-	); ?>
-</div>
-<div class="row">
-	<div class="small-12 content-wrapper featured-story columns">
 		<?php
-		if ( is_active_sidebar( 'homepage_featured_story' ) ) :
-			dynamic_sidebar( 'homepage_featured_story' );
+		do_action( 'above-homepage-headlines' );
+		if ( is_active_sidebar( 'homepage_headlines' ) ) :
+			dynamic_sidebar( 'homepage_headlines' );
 		endif;
 		?>
-	</div>
-	<div class="columns">
-		<hr class="before">
-		<h2 class="section-title"><span><?php esc_html_e( 'More From', 'chicagosuntimes' ); ?></span></h2>
-		<hr/>
-	</div>
-</div>
-<!-- circular flipp -->
-	<div class="row">
-		<?php
-		if ( is_active_sidebar( 'undermorefrom' ) ) :
-			dynamic_sidebar( 'undermorefrom' );
-		endif;
-		?>
-	</div>
-	<div>
 		<div class="large-12 dfp-atf-leaderboard">
-			<?php echo wp_kses( CST()->dfp_handler->unit( 4, 'div-gpt-super-leaderboard', 'dfp dfp-super-leaderboard dfp-centered' ),
+			<?php echo wp_kses( CST()->dfp_handler->unit( 3, 'div-gpt-super-leaderboard', 'dfp dfp-super-leaderboard dfp-centered' ),
 				CST()->dfp_kses
 			); ?>
 		</div>
-	</div>
-	<div class="row">
-		<div class="large-12 columns content-wrapper cw">
-			<?php get_template_part( 'parts/homepage/column-wells' ); ?>
+		<div class="row">
+			<div class="small-12 content-wrapper featured-story columns">
+				<?php
+				if ( is_active_sidebar( 'homepage_featured_story' ) ) :
+					dynamic_sidebar( 'homepage_featured_story' );
+				endif;
+				?>
+			</div>
+			<div class="columns">
+				<hr class="before">
+				<h2 class="section-title"><span><?php esc_html_e( 'More From', 'chicagosuntimes' ); ?></span></h2>
+				<hr/>
+			</div>
+		</div>
+		<!-- circular flipp -->
+		<div class="row">
+			<?php
+			if ( is_active_sidebar( 'undermorefrom' ) ) :
+				dynamic_sidebar( 'undermorefrom' );
+			endif;
+			?>
+		</div>
+		<div>
+			<div class="large-12 dfp-atf-leaderboard">
+				<?php echo wp_kses( CST()->dfp_handler->unit( 4, 'div-gpt-super-leaderboard', 'dfp dfp-super-leaderboard dfp-centered' ),
+					CST()->dfp_kses
+				); ?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="large-12 columns content-wrapper cw">
+				<?php get_template_part( 'parts/homepage/column-wells' ); ?>
+			</div>
 		</div>
 	</div>
-</div>
 <?php get_template_part( 'parts/homepage/footer' ); ?>
 <?php get_footer();
