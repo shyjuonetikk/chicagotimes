ca839271c622a8bcea700f0c3809ede30f55ea82
Sean Hayes <seanchayes@gmail.com>
2017-04-27 13:31:11 -0500

    More layout changes to mirror the source

--- parts/homepage/main-wells-v3.php
+++ parts/homepage/main-wells-v3.php
@@ -3,46 +3,66 @@
 		<div class="row">
 			<div class="columns small-12 medium-6 large-4">
 				<span class="lead-story">
-					<span class="lead-story-title">
+					<span class="title">
 						<h2>Missing 1-year-old girl found dead in her Joliet home</h2>
 					</span>
-					<span class="lead-story-excerpt">
-						A missing baby girl who vanished Tuesday evening was found dead late Wednesday in a home in “deplorable condition” near southwest suburban Joliet
+					<span class="image">
+						<img src="http://placehold.it/330x250&amp;text=[optional lead Story]">
+					</span>
+					<span class="excerpt">
+						<p>A missing baby girl who vanished Tuesday evening was found dead late Wednesday in a home in “deplorable condition” near southwest suburban Joliet</p>
+					</span>
+					<span class="authors">
+						<p>By Clark Kent and Jimmy Olsen - 2 hours ago</p>
 					</span>
 				</span>
-				<ul class="lead-related-stories-collection">
-					<li><a href="#" class="lead-related-link">Related story link 1 <i class="fa fa-play"></i></a></li>
-					<li><a href="#" class="lead-related-link">Related story link 2 <i class="fa fa-play"></i></a></li>
-					<li><a href="#" class="lead-related-link">Related story link 3 <i class="fa fa-play"></i></a></li>
-					<li><a href="#" class="lead-related-link">Related story link 4 <i class="fa fa-play"></i></a></li>
-					<li><a href="#" class="lead-related-link">Related story link 5 <i class="fa fa-play"></i></a></li>
-				</ul>
 			</div>
 			<div class="columns small-12 medium-6 large-8">
 				<ul class="small-block-grid-1 large-block-grid-2">
 					<li class="columns small-12">
 						<span class="mini-story">
-							<img class="mini-story-image" src="http://placehold.it/80x80&amp;text=[mini Story]">
+							<span class="image">
+								<img src="http://placehold.it/60x60&amp;text=[mini Story]">
+							</span>
 							<span class="mini-story-title"><h2>Trump unveils tax plan: Three income brackets, top rate lowered</h2></span>
+							<span class="authors">
+								<p>By Clark Kent and Jimmy Olsen - 2 hours ago</p>
+							</span>
 						</span>
 					</li>
 					<li class="columns small-12">
 						<span class="mini-story">
-							<img class="mini-story-image" src="http://placehold.it/80x80&amp;text=[mini Story]">
+							<span class="image">
+								<img src="http://placehold.it/60x60&amp;text=[mini Story]">
+							</span>
 							<span class="mini-story-title"><h2>Jesse Jackson Jr. dismisses divorce case in Chicago</h2></span>
-						</span>
+							<span class="authors">
+								<p>By Clark Kent and Jimmy Olsen - 3 hours ago</p>
+							</span>
+					</span>
 					</li>
 					<li class="columns small-12">
 						<span class="mini-story">
-							<img class="mini-story-image" src="http://placehold.it/80x80&amp;text=[mini Story]">
+							<span class="image">
+								<img src="http://placehold.it/60x60&amp;text=[mini Story]">
+							</span>
 							<span class="mini-story-title"><h2>Brown: Summers' floating of own candidacy could buoy Pritzker</h2></span>
-						</span>
+							<span class="authors">
+								<p>By Clark Kent and Jimmy Olsen - 4 hours ago</p>
+							</span>
+					</span>
 					</li>
 					<li class="columns small-12">
 						<span class="mini-story">
-							<img class="mini-story-image" src="http://placehold.it/80x80&amp;text=[mini Story]">
+							<span class="image">
+								<img src="http://placehold.it/60x60&amp;text=[mini Story]">
+							</span>
 							<span class="mini-story-title"><h2>Rajon Rondo is still out for Game 5 — and shoots down report</h2></span>
+							<span class="authors">
+								<p>By Clark Kent and Jimmy Olsen - 1 day ago</p>
+							</span>
 						</span>
+
 					</li>
 				</ul>
 			</div>
