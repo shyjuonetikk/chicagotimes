22ea5cfbdd2928696d600004203b3e417f0d687b
Sean Hayes <seanchayes@gmail.com>
2017-04-27 12:38:48 -0500

    Setting anchor links to blue, widget multi device display layout testing

--- parts/homepage/main-wells-v3.php
+++ parts/homepage/main-wells-v3.php
@@ -1,39 +1,52 @@
 <div class="row">
-	<div class="columns small-12 medium-8 stories">
+	<div class="columns small-12 medium-8 large-9 stories">
 		<div class="row">
-			<div class="columns small-12 medium-4">
-				<img src="http://placehold.it/330x300&amp;text=[Story]">
-				<ul>
-					<li>Bullet point 1</li>
-					<li>Bullet point 2</li>
-					<li>Bullet point 3</li>
-					<li>Bullet point 4</li>
-					<li>Bullet point 5</li>
+			<div class="columns small-12 medium-6 large-4">
+				<span class="lead-story">
+					<img src="http://placehold.it/330x300&amp;text=[Lead Story]">
+					<span class="lead-story-title">
+						<h2>Missing 1-year-old girl found dead in her Joliet home</h2>
+					</span>
+				</span>
+				<ul class="lead-related-stories-collection">
+					<li><a href="#" class="lead-related-link">Related story link 1 <i class="fa fa-play"></i></a></li>
+					<li><a href="#" class="lead-related-link">Related story link 2 <i class="fa fa-play"></i></a></li>
+					<li><a href="#" class="lead-related-link">Related story link 3 <i class="fa fa-play"></i></a></li>
+					<li><a href="#" class="lead-related-link">Related story link 4 <i class="fa fa-play"></i></a></li>
+					<li><a href="#" class="lead-related-link">Related story link 5 <i class="fa fa-play"></i></a></li>
 				</ul>
 			</div>
-			<div class="columns small-12 medium-8">
-				<div class="row">
-					<ul class="small-block-grid-4">
-						<li class="columns small-12">
-							<img src="http://placehold.it/80x80&amp;text=[Story]">
-							<h2>Trump unveils tax plan: Three income brackets, top rate lowered</h2>
-						</li>
-						<li class="columns small-12">
-							<img src="http://placehold.it/80x80&amp;text=[Story]">
-							<h2>Jesse Jackson Jr. dismisses divorce case in Chicago</h2>
-						</li>
-						<li class="columns small-12">
-							<img src="http://placehold.it/80x80&amp;text=[Story]">
-							<h2>Brown: Summers' floating of own candidacy could buoy Pritzker</h2>
-						</li>
-						<li class="columns small-12">
-							<img src="http://placehold.it/80x80&amp;text=[Story]">
-							<h2>Rajon Rondo is still out for Game 5 — and shoots down report</h2>
-						</li>
-					</ul>
-				</div>
+			<div class="columns small-12 medium-6 large-8">
+				<ul class="small-block-grid-1 large-block-grid-2">
+					<li class="columns small-12">
+						<span class="mini-story">
+							<img class="mini-story-image" src="http://placehold.it/80x80&amp;text=[mini Story]">
+							<span class="mini-story-title"><h2>Trump unveils tax plan: Three income brackets, top rate lowered</h2></span>
+						</span>
+					</li>
+					<li class="columns small-12">
+						<span class="mini-story">
+							<img class="mini-story-image" src="http://placehold.it/80x80&amp;text=[mini Story]">
+							<span class="mini-story-title"><h2>Jesse Jackson Jr. dismisses divorce case in Chicago</h2></span>
+						</span>
+					</li>
+					<li class="columns small-12">
+						<span class="mini-story">
+							<img class="mini-story-image" src="http://placehold.it/80x80&amp;text=[mini Story]">
+							<span class="mini-story-title"><h2>Brown: Summers' floating of own candidacy could buoy Pritzker</h2></span>
+						</span>
+					</li>
+					<li class="columns small-12">
+						<span class="mini-story">
+							<img class="mini-story-image" src="http://placehold.it/80x80&amp;text=[mini Story]">
+							<span class="mini-story-title"><h2>Rajon Rondo is still out for Game 5 — and shoots down report</h2></span>
+						</span>
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
-	<div class="columns small-12 medium-4 sidebar"><img src="http://placehold.it/300x600&amp;text=[ad]"></div>
+	<div class="columns small-11 medium-4 large-3 sidebar small-centered medium-uncentered">
+		<img src="http://placehold.it/300x600&amp;text=[ad]">
+	</div>
 </div>
