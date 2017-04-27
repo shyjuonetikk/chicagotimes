09572d2cffd7aaca582491a6af9f626572f2ed45
Sean Hayes <seanchayes@gmail.com>
2017-04-27 15:54:51 -0500

    More HTML and CSS markup and styling changes to better resemble the source

--- parts/homepage/main-wells-v3.php
+++ parts/homepage/main-wells-v3.php
@@ -1,13 +1,13 @@
-<div class="row">
+<div class="row stories-container">
 	<div class="columns small-12 medium-8 large-9 stories">
-		<div class="row">
+		<div class="row" data-equalizer-mq="large-up">
 			<div class="columns small-12 medium-6 large-4">
 				<span class="lead-story">
 					<span class="title">
-						<h2>Missing 1-year-old girl found dead in her Joliet home</h2>
+						<h3>Missing 1-year-old girl found dead in her Joliet home</h3>
 					</span>
-					<span class="image">
-						<img src="http://placehold.it/330x250&amp;text=[optional lead Story]">
+					<span class="image show-for-large-down">
+						<img src="http://placehold.it/394x263&amp;text=[394x263 optional lead Story]">
 					</span>
 					<span class="excerpt">
 						<p>A missing baby girl who vanished Tuesday evening was found dead late Wednesday in a home in “deplorable condition” near southwest suburban Joliet</p>
@@ -15,56 +15,88 @@
 					<span class="authors">
 						<p>By Clark Kent and Jimmy Olsen - 2 hours ago</p>
 					</span>
+					<ul class="related-title">
+						<li><a href="#"><h3>Analysis: When did Trump declare the wall will be built?</h3></a></li>
+					</ul>
+				</span>
+				<span class="lead-story">
+					<span class="title">
+						<h3>Sandi Jackson to Jesse Jr.: List all sex partners—names and dates</h3>
+					</span>
+					<span class="image show-for-large-down">
+						<img src="https://suntimesmedia.files.wordpress.com/2017/04/jackson_resignation_30508923-e1493239957893.jpg?w=394">
+					</span>
+					<span class="excerpt">
+						<p>Sandi Jackson to Jesse Jr.: List all sex partners—names and dates</p>
+					</span>
+					<span class="authors">
+						<p>By Tina Sfondeles - 1 hour ago</p>
+					</span>
+				</span>
+				<span class="lead-story">
+					<span class="title">
+						<h3>9-year sentence for Rockford-area doctor Charles Dehaan</h3>
+					</span>
+					<span class="image show-for-large-down">
+						<img src="https://suntimesmedia.files.wordpress.com/2016/09/doccharlesdehaan092216.jpg?w=394">
+					</span>
+					<span class="excerpt">
+						<p>Charles DeHaan got nine years in prison for fraudulently billing Medicare at house calls where prosecutors allege he sexually assaulted patients.</p>
+					</span>
+					<span class="authors">
+						<p>By Andy Grimm - 1 hour ago</p>
+					</span>
 				</span>
 			</div>
 			<div class="columns small-12 medium-6 large-8">
-				<ul class="small-block-grid-1 large-block-grid-2">
-					<li class="columns small-12">
-						<span class="mini-story">
+				<div class="row mini-stories" data-equalizer>
+					<div class="columns small-12 medium-6" data-equalizer-watch>
 							<span class="image">
-								<img src="http://placehold.it/60x60&amp;text=[mini Story]">
+								<img src="https://suntimesmedia.files.wordpress.com/2017/04/673416790_68461575.jpg?w=80">
 							</span>
-							<span class="mini-story-title"><h2>Trump unveils tax plan: Three income brackets, top rate lowered</h2></span>
+							<span class="title"><h3>Trump unveils tax plan: Three income brackets, top rate lowered</h3></span>
 							<span class="authors">
 								<p>By Clark Kent and Jimmy Olsen - 2 hours ago</p>
 							</span>
-						</span>
-					</li>
-					<li class="columns small-12">
-						<span class="mini-story">
+					</div>
+					<div class="columns small-12 medium-6" data-equalizer-watch>
 							<span class="image">
-								<img src="http://placehold.it/60x60&amp;text=[mini Story]">
+								<img src="https://suntimesmedia.files.wordpress.com/2017/01/sneedmccarthy012717.jpg?w=80">
 							</span>
-							<span class="mini-story-title"><h2>Jesse Jackson Jr. dismisses divorce case in Chicago</h2></span>
+							<span class="title"><h3>Jesse Jackson Jr. dismisses divorce case in Chicago</h3></span>
 							<span class="authors">
 								<p>By Clark Kent and Jimmy Olsen - 3 hours ago</p>
 							</span>
-					</span>
-					</li>
-					<li class="columns small-12">
-						<span class="mini-story">
+					</div>
+					<div class="columns small-12 medium-6" data-equalizer-watch>
 							<span class="image">
-								<img src="http://placehold.it/60x60&amp;text=[mini Story]">
+								<img src="https://suntimesmedia.files.wordpress.com/2017/04/summers-042617-5.jpg?w=80">
 							</span>
-							<span class="mini-story-title"><h2>Brown: Summers' floating of own candidacy could buoy Pritzker</h2></span>
+							<span class="title"><h3>Brown: Summers' floating of own candidacy could buoy Pritzker</h3></span>
 							<span class="authors">
 								<p>By Clark Kent and Jimmy Olsen - 4 hours ago</p>
 							</span>
-					</span>
-					</li>
-					<li class="columns small-12">
-						<span class="mini-story">
+					</div>
+					<div class="columns small-12 medium-6" data-equalizer-watch>
 							<span class="image">
-								<img src="http://placehold.it/60x60&amp;text=[mini Story]">
+								<img src="https://suntimesmedia.files.wordpress.com/2017/04/rondo13.jpg?w=80">
 							</span>
-							<span class="mini-story-title"><h2>Rajon Rondo is still out for Game 5 — and shoots down report</h2></span>
+							<span class="title"><h3>Rajon Rondo is still out for Game 5 — and shoots down report</h3></span>
 							<span class="authors">
 								<p>By Clark Kent and Jimmy Olsen - 1 day ago</p>
 							</span>
-						</span>
-
-					</li>
-				</ul>
+					</div>
+				</div>
+				<div class="other-stories">
+					<h2>Also in the Chicago Sun-Times</h2>
+					<ul class="list">
+						<li><span class="section-name"><a href="#" class="section-link">Chicago News</a></span> <a href="#">Mentally ill woman gets 22 years for killing husband with poison</a></li>
+						<li><span class="section-name"><a href="#" class="section-link">Chicago News</a></span> <a href="#">9 charged with Crystal Lake fight that led to stabbing</a></li>
+						<li><span class="section-name"><a href="#" class="section-link">Chicago Sports</a></span> <a href="#">Anthony Swarzak gettung career back on track with White Sox</a></li>
+						<li><span class="section-name"><a href="#" class="section-link">Entertainment</a></span> <a href="#">Dear Abby: My friend bullies other kids at school</a></li>
+						<li><span class="section-name"><a href="#" class="section-link">Chicago Politics</a></span> <a href="#">Sneed exclusive: City could deal blow to Trump wall contractors</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
