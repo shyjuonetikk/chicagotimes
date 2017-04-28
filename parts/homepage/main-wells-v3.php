86c5b1c7e45539d4abfdbedf5e774a36b5a98659
Sean Hayes <seanchayes@gmail.com>
2017-04-28 15:54:53 -0500

    Improved layout in upper and lower news wells, requires styling in lower well, needs ad as a separator too

--- parts/homepage/main-wells-v3.php
+++ parts/homepage/main-wells-v3.php
@@ -4,11 +4,11 @@
 			<div class="columns small-12 medium-6 large-4">
 				<div class="lead-story">
 					<h3 class="title">Missing 1-year-old girl found dead in her Joliet home</h3>
-					<span class="image show-for-medium-down">
+					<span class="image show-for-landscape hidden-for-large-only show-for-xlarge-up">
 						<img src="https://suntimesmedia.files.wordpress.com/2017/04/semajcrosbyyellowhouse.jpg?w=394">
 					</span>
 					<p class="excerpt">
-						<span class="image show-for-large-up">
+						<span class="image show-for-large-up show-for-touch">
 							<img src="https://suntimesmedia.files.wordpress.com/2017/04/semajcrosbyyellowhouse.jpg?w=80" alt="">
 						</span>
 						A missing baby girl who vanished Tuesday evening was found dead late Wednesday in a home in “deplorable condition” near southwest suburban Joliet
@@ -20,7 +20,7 @@
 				</div>
 				<div class="lead-story">
 						<h3 class="title">Sandi Jackson to Jesse Jr.: List all sex partners—names and dates</h3>
-					<span class="image show-for-medium-down">
+					<span class="image show-for-landscape hidden-for-medium-up show-for-xlarge-up">
 						<img src="https://suntimesmedia.files.wordpress.com/2017/04/jackson_resignation_30508923-e1493239957893.jpg?w=394">
 					</span>
 						<p class="excerpt">Sandi Jackson to Jesse Jr.: List all sex partners—names and dates
@@ -29,11 +29,11 @@
 				</div>
 				<div class="lead-story">
 					<h3 class="title">9-year sentence for Rockford-area doctor Charles Dehaan</h3>
-					<span class="image show-for-medium-down">
+					<span class="image show-for-landscape hidden-for-medium-up show-for-xlarge-up">
 						<img src="https://suntimesmedia.files.wordpress.com/2016/09/doccharlesdehaan092216.jpg?w=394">
 					</span>
 					<p class="excerpt">
-					<span class="image show-for-large-up">
+					<span class="image show-for-large-up show-for-touch">
 						<img src="https://suntimesmedia.files.wordpress.com/2016/09/doccharlesdehaan092216.jpg?w=80">
 					</span>
 						Charles DeHaan got nine years in prison for fraudulently billing Medicare at house calls where prosecutors allege he sexually assaulted patients.</p>
@@ -57,35 +57,59 @@
 				<hr>
 				<div class="row mini-stories" data-equalizer>
 					<div class="columns small-12 medium-12 large-6" data-equalizer-watch>
-						<span class="image">
-							<img src="https://suntimesmedia.files.wordpress.com/2017/04/673416790_68461575.jpg?w=80">
-						</span>
-						<h3 class="title">Trump unveils tax plan: Three income brackets, top rate lowered</h3>
-						<p class="authors">By Clark Kent and Jimmy Olsen - 2 hours ago</p>
+						<div class="row">
+							<div class="columns small-12 medium-4 large-4">
+								<span class="image">
+									<img src="https://suntimesmedia.files.wordpress.com/2017/04/673416790_68461575.jpg?w=80">
+								</span>
+							</div>
+							<div class="columns small-12 medium-8 large-8">
+								<h3 class="title">Trump unveils tax plan: Three income brackets, top rate lowered</h3>
+							</div>
+							<div class="columns small-12"><p class="authors">By Clark Kent and Jimmy Olsen - 2 hours ago</p></div>
+						</div>
 					</div>
 					<div class="columns small-12 medium-12 large-6" data-equalizer-watch>
-						<span class="image">
-							<img src="https://suntimesmedia.files.wordpress.com/2017/01/sneedmccarthy012717.jpg?w=80">
-						</span>
-						<h3 class="title">Jesse Jackson Jr. dismisses divorce case in Chicago</h3>
-						<p class="authors">By Clark Kent and Jimmy Olsen - 3 hours ago</p>
+						<div class="row">
+						<div class="columns small-12 medium-4 large-4">
+							<span class="image">
+								<img src="https://suntimesmedia.files.wordpress.com/2017/01/sneedmccarthy012717.jpg?w=80">
+							</span>
+						</div>
+						<div class="columns small-12 medium-8 large-8">
+							<h3 class="title">Jesse Jackson Jr. dismisses divorce case in Chicago</h3>
+						</div>
+							<div class="columns small-12"><p class="authors">By Clark Kent and Jimmy Olsen - 3 hours ago</p></div>
+						</div>
 					</div>
 					<div class="columns small-12 medium-12 large-6" data-equalizer-watch>
+						<div class="row">
+						<div class="columns small-12 medium-4 large-4">
 						<span class="image">
 							<img src="https://suntimesmedia.files.wordpress.com/2017/04/summers-042617-5.jpg?w=80">
 						</span>
-						<h3 class="title">Brown: Summers' floating of own candidacy could buoy Pritzker</h3>
-						<p class="authors">By Clark Kent and Jimmy Olsen - 4 hours ago</p>
+						</div>
+						<div class="columns small-12 medium-8 large-8">
+							<h3 class="title">Brown: Summers' floating of own candidacy could buoy Pritzker</h3>
+						</div>
+							<div class="columns small-12"><p class="authors">By Clark Kent and Jimmy Olsen - 4 hours ago</p></div>
+						</div>
 					</div>
 					<div class="columns small-12 medium-12 large-6" data-equalizer-watch>
+						<div class="row">
+						<div class="columns small-12 medium-4 large-4">
 						<span class="image">
 							<img src="https://suntimesmedia.files.wordpress.com/2017/04/rondo13.jpg?w=80">
 						</span>
-						<h3 class="title">Rajon Rondo is still out for Game 5 — and shoots down report</h3>
-						<p class="authors">By Clark Kent and Jimmy Olsen - 1 day ago</p>
+						</div>
+						<div class="columns small-12 medium-8 large-8">
+							<h3 class="title">Rajon Rondo is still out for Game 5 — and shoots down report</h3>
+						</div>
+							<div class="columns small-12"><p class="authors">By Clark Kent and Jimmy Olsen - 1 day ago</p></div>
+						</div>
 					</div>
 				</div>
-				<div class="other-stories">
+				<div class="other-stories show-for-large-up">
 					<h2>Also in the Chicago Sun-Times</h2>
 					<ul class="list">
 						<li><span class="section-name"><a href="#" class="section-link">Chicago News</a></span> <a href="#">Mentally ill woman gets 22 years for killing husband with poison</a></li>
@@ -100,6 +124,20 @@
 	</div>
 	<div class="columns small-11 medium-4 large-3 sidebar small-centered medium-uncentered">
 		<img src="http://placehold.it/300x600&amp;text=[ad]">
+		<div class="stories-container hide-for-large-up">
+			<div class="stories">
+				<div class="other-stories">
+					<h2>Also in the Chicago Sun-Times</h2>
+					<ul class="list">
+						<li><span class="section-name"><a href="#" class="section-link">Chicago News</a></span> <a href="#">Mentally ill woman gets 22 years for killing husband with poison</a></li>
+						<li><span class="section-name"><a href="#" class="section-link">Chicago News</a></span> <a href="#">9 charged with Crystal Lake fight that led to stabbing</a></li>
+						<li><span class="section-name"><a href="#" class="section-link">Chicago Sports</a></span> <a href="#">Anthony Swarzak gettung career back on track with White Sox</a></li>
+						<li><span class="section-name"><a href="#" class="section-link">Entertainment</a></span> <a href="#">Dear Abby: My friend bullies other kids at school</a></li>
+						<li><span class="section-name"><a href="#" class="section-link">Chicago Politics</a></span> <a href="#">Sneed exclusive: City could deal blow to Trump wall contractors</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
 	<hr>
 </div>
--- parts/homepage/more-wells-v3.php
+++ parts/homepage/more-wells-v3.php
@@ -3,17 +3,13 @@
 	<div class="columns small-12 medium-8 large-9">
 		<div class="row">
 		<div class="columns small-12 medium-6 large-4">
-			<div class="section-column-wells">
 			<h3 class="more-sub-head">Latest Stories</h3>
+			<div class="section-column-wells">
 				<div class="section-list">
-					<h3 class="<?php echo esc_attr( $section_slug ); ?>-border">
-						<a href="<?php echo esc_url( home_url( '/' ) . 'section/' . esc_attr( $section_slug ) . '/' ); ?>" data-on="click" data-event-category="navigation"
-																				   data-event-action="navigate-hp-<?php echo esc_attr( $section_slug ); ?>-column-title">
-							Chicago <?php esc_html_e( ucfirst( $section_slug ), 'chicagosuntimes' ); ?></a></h3>
 					<?php $query = array(
 						'post_type'           => array( 'cst_article' ),
 						'ignore_sticky_posts' => true,
-						'posts_per_page'      => 5,
+						'posts_per_page'      => 10,
 						'post_status'         => 'publish',
 						'cst_section'         => esc_attr( $section_slug ),
 						'orderby'             => 'modified',
@@ -22,23 +18,26 @@
 				</div>
 			</div>
 		</div>
-		<div class="columns small-12 medium-6 large-8"><h3 class="more-sub-head"><a href="<?php echo esc_url( home_url( '/' ) ); ?>features/"></a>Features</h3>
+		<div class="columns small-12 medium-6 large-8">
 			<div class="small-12 large-8 columns">
-				<div class="row featured-story">
+			<div class="row">
+				<h3 class="more-sub-head"><a href="<?php echo esc_url( home_url( '/' ) ); ?>features/"></a>Features</h3>
+				<div class="featured-story">
 					<a href="http://chicago.suntimes.com/feature/50-years-after-chicago-areas-most-devastating-tornadoes/" target="_blank" data-on="click" data-event-category="navigation" data-event-action="navigate-hp-featured-story">
 						<img src="https://suntimesmedia.files.wordpress.com/2017/04/tornado-041617-01_68208979.jpg?w=394" alt="article promo image">
 						<h3>Survivors' stories 50 years after Chicago area's deadliest tornadoes hit Oak Lawn, other towns</h3>
 					</a>
 				</div>
 			</div>
+			</div>
 			<div class="columns small-12 large-4">
 				<div class="row">
+				<h3 class="more-sub-head">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>features/" data-on="click" data-event-category="navigation"
+					   data-event-action="navigate-hp-features-column-title">
+						More Features</a></h3>
 					<div class="section-column-wells">
 						<div class="section-list">
-							<h3 class="news-border">
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>features/" data-on="click" data-event-category="navigation"
-								   data-event-action="navigate-hp-features-column-title">
-									More Features</a></h3>
 					<?php $query = array(
 						'post_type'           => array( 'cst_feature' ),
 						'ignore_sticky_posts' => true,
@@ -51,19 +50,84 @@
 					</div>
 				</div>
 			</div>
+			<hr>
+			<div class="columns small-12">
+				<div class="row">
+					<div class="section-column-wells">
+						<h3 class="more-sub-head">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>features/" data-on="click" data-event-category="navigation"
+							   data-event-action="navigate-hp-features-column-title">
+								Entertainment</a></h3>
+						<div class="row mini-stories" data-equalizer>
+							<div class="columns small-12 medium-12 large-6" data-equalizer-watch>
+								<div class="row">
+									<div class="columns small-12 medium-4 large-4">
+								<span class="image">
+									<img src="https://suntimesmedia.files.wordpress.com/2017/04/673416790_68461575.jpg?w=80">
+								</span>
+									</div>
+									<div class="columns small-12 medium-8 large-8">
+										<h3 class="title">Trump unveils tax plan: Three income brackets, top rate lowered</h3>
+									</div>
+									<div class="columns small-12"><p class="authors">By Clark Kent and Jimmy Olsen - 2 hours ago</p></div>
+								</div>
+							</div>
+							<div class="columns small-12 medium-12 large-6" data-equalizer-watch>
+								<div class="row">
+									<div class="columns small-12 medium-4 large-4">
+							<span class="image">
+								<img src="https://suntimesmedia.files.wordpress.com/2017/01/sneedmccarthy012717.jpg?w=80">
+							</span>
+									</div>
+									<div class="columns small-12 medium-8 large-8">
+										<h3 class="title">Jesse Jackson Jr. dismisses divorce case in Chicago</h3>
+									</div>
+									<div class="columns small-12"><p class="authors">By Clark Kent and Jimmy Olsen - 3 hours ago</p></div>
+								</div>
+							</div>
+							<div class="columns small-12 medium-12 large-6" data-equalizer-watch>
+								<div class="row">
+									<div class="columns small-12 medium-4 large-4">
+						<span class="image">
+							<img src="https://suntimesmedia.files.wordpress.com/2017/04/summers-042617-5.jpg?w=80">
+						</span>
+									</div>
+									<div class="columns small-12 medium-8 large-8">
+										<h3 class="title">Brown: Summers' floating of own candidacy could buoy Pritzker</h3>
+									</div>
+									<div class="columns small-12"><p class="authors">By Clark Kent and Jimmy Olsen - 4 hours ago</p></div>
+								</div>
+							</div>
+							<div class="columns small-12 medium-12 large-6" data-equalizer-watch>
+								<div class="row">
+									<div class="columns small-12 medium-4 large-4">
+						<span class="image">
+							<img src="https://suntimesmedia.files.wordpress.com/2017/04/rondo13.jpg?w=80">
+						</span>
+									</div>
+									<div class="columns small-12 medium-8 large-8">
+										<h3 class="title">Rajon Rondo is still out for Game 5 — and shoots down report</h3>
+									</div>
+									<div class="columns small-12"><p class="authors">By Clark Kent and Jimmy Olsen - 1 day ago</p></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		</div>
 	</div>
 	<div class="columns small-11 medium-4 large-3 sidebar small-centered medium-uncentered">
 		<img src="http://placehold.it/300x250&amp;text=[ad]">
+		<hr>
 		<div class="section-column-wells">
-			<h3 class="more-sub-head">Opinion</h3>
 			<?php $section_slug = 'opinion'; ?>
+			<h3 class="more-sub-head">
+				<a href="<?php echo esc_url( home_url( '/' ) . 'section/' . esc_attr( $section_slug ) . '/' ); ?>" data-on="click" data-event-category="navigation"
+				   data-event-action="navigate-hp-<?php echo esc_attr( $section_slug ); ?>-column-title">
+					<?php esc_html_e( ucfirst( $section_slug ), 'chicagosuntimes' ); ?></a></h3>
 				<div class="section-list">
-					<h3 class="<?php echo esc_attr( $section_slug ); ?>-border">
-						<a href="<?php echo esc_url( home_url( '/' ) . 'section/' . esc_attr( $section_slug ) . '/' ); ?>" data-on="click" data-event-category="navigation"
-						   data-event-action="navigate-hp-<?php echo esc_attr( $section_slug ); ?>-column-title">
-							<?php esc_html_e( ucfirst( $section_slug ), 'chicagosuntimes' ); ?></a></h3>
 					<?php $query = array(
 						'post_type'           => array( 'cst_article' ),
 						'ignore_sticky_posts' => true,
