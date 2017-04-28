6675a51616b244ff9661f5553f8f43d85b10ebc6
Sean Hayes <seanchayes@gmail.com>
2017-04-28 09:02:12 -0500

    Main well fleshing out, inclusion of small images, lead mini story

--- parts/homepage/main-wells-v3.php
+++ parts/homepage/main-wells-v3.php
@@ -2,89 +2,87 @@
 	<div class="columns small-12 medium-8 large-9 stories">
 		<div class="row" data-equalizer-mq="large-up">
 			<div class="columns small-12 medium-6 large-4">
-				<span class="lead-story">
-					<span class="title">
-						<h3>Missing 1-year-old girl found dead in her Joliet home</h3>
+				<div class="lead-story">
+					<h3 class="title">Missing 1-year-old girl found dead in her Joliet home</h3>
+					<span class="image show-for-medium-down">
+						<img src="https://suntimesmedia.files.wordpress.com/2017/04/semajcrosbyyellowhouse.jpg?w=394">
 					</span>
-					<span class="image show-for-large-down">
-						<img src="http://placehold.it/394x263&amp;text=[394x263 optional lead Story]">
-					</span>
-					<span class="excerpt">
-						<p>A missing baby girl who vanished Tuesday evening was found dead late Wednesday in a home in “deplorable condition” near southwest suburban Joliet</p>
-					</span>
-					<span class="authors">
-						<p>By Clark Kent and Jimmy Olsen - 2 hours ago</p>
-					</span>
+					<p class="excerpt">
+						<span class="image show-for-large-up">
+							<img src="https://suntimesmedia.files.wordpress.com/2017/04/semajcrosbyyellowhouse.jpg?w=80" alt="">
+						</span>
+						A missing baby girl who vanished Tuesday evening was found dead late Wednesday in a home in “deplorable condition” near southwest suburban Joliet
+					</p>
+					<p class="authors">By Clark Kent and Jimmy Olsen - 2 hours ago</p>
 					<ul class="related-title">
 						<li><a href="#"><h3>Analysis: When did Trump declare the wall will be built?</h3></a></li>
 					</ul>
-				</span>
-				<span class="lead-story">
-					<span class="title">
-						<h3>Sandi Jackson to Jesse Jr.: List all sex partners—names and dates</h3>
-					</span>
-					<span class="image show-for-large-down">
+				</div>
+				<div class="lead-story">
+						<h3 class="title">Sandi Jackson to Jesse Jr.: List all sex partners—names and dates</h3>
+					<span class="image show-for-medium-down">
 						<img src="https://suntimesmedia.files.wordpress.com/2017/04/jackson_resignation_30508923-e1493239957893.jpg?w=394">
 					</span>
-					<span class="excerpt">
-						<p>Sandi Jackson to Jesse Jr.: List all sex partners—names and dates</p>
-					</span>
-					<span class="authors">
-						<p>By Tina Sfondeles - 1 hour ago</p>
-					</span>
-				</span>
-				<span class="lead-story">
-					<span class="title">
-						<h3>9-year sentence for Rockford-area doctor Charles Dehaan</h3>
-					</span>
-					<span class="image show-for-large-down">
+						<p class="excerpt">Sandi Jackson to Jesse Jr.: List all sex partners—names and dates
+						</p>
+						<p class="authors">By Tina Sfondeles - 1 hour ago</p>
+				</div>
+				<div class="lead-story">
+					<h3 class="title">9-year sentence for Rockford-area doctor Charles Dehaan</h3>
+					<span class="image show-for-medium-down">
 						<img src="https://suntimesmedia.files.wordpress.com/2016/09/doccharlesdehaan092216.jpg?w=394">
 					</span>
-					<span class="excerpt">
-						<p>Charles DeHaan got nine years in prison for fraudulently billing Medicare at house calls where prosecutors allege he sexually assaulted patients.</p>
+					<p class="excerpt">
+					<span class="image show-for-large-up">
+						<img src="https://suntimesmedia.files.wordpress.com/2016/09/doccharlesdehaan092216.jpg?w=80">
 					</span>
-					<span class="authors">
-						<p>By Andy Grimm - 1 hour ago</p>
-					</span>
-				</span>
+						Charles DeHaan got nine years in prison for fraudulently billing Medicare at house calls where prosecutors allege he sexually assaulted patients.</p>
+					<p class="authors">By Andy Grimm - 1 hour ago</p>
+				</div>
 			</div>
 			<div class="columns small-12 medium-6 large-8">
+				<div class="row lead-mini-story">
+					<div class="columns small-12">
+						<div class="row">
+							<div class="columns small-12 medium-6">
+								<span class="image"><img src="https://suntimesmedia.files.wordpress.com/2017/04/sir_the_baptist-09_68228313.jpg?w=300" alt=""></span></div>
+							<div class="columns small-12 medium-6">
+								<h3 class="title">Sir the Baptist: ‘I want to be the first hip-hop chaplain’</h3>
+								<p class="excerpt">Sir the Baptist is aiming to reconnect “the hip-hop world to spirituality” and “spirituality to common sense.” And listen to ‘Face to Faith’ podcast.</p>
+								<p class="authors">By Robert Herguth - 1/2 hour ago</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<hr>
 				<div class="row mini-stories" data-equalizer>
 					<div class="columns small-12 medium-6" data-equalizer-watch>
-							<span class="image">
-								<img src="https://suntimesmedia.files.wordpress.com/2017/04/673416790_68461575.jpg?w=80">
-							</span>
-							<span class="title"><h3>Trump unveils tax plan: Three income brackets, top rate lowered</h3></span>
-							<span class="authors">
-								<p>By Clark Kent and Jimmy Olsen - 2 hours ago</p>
-							</span>
+						<span class="image">
+							<img src="https://suntimesmedia.files.wordpress.com/2017/04/673416790_68461575.jpg?w=80">
+						</span>
+						<h3 class="title">Trump unveils tax plan: Three income brackets, top rate lowered</h3>
+						<p class="authors">By Clark Kent and Jimmy Olsen - 2 hours ago</p>
 					</div>
 					<div class="columns small-12 medium-6" data-equalizer-watch>
-							<span class="image">
-								<img src="https://suntimesmedia.files.wordpress.com/2017/01/sneedmccarthy012717.jpg?w=80">
-							</span>
-							<span class="title"><h3>Jesse Jackson Jr. dismisses divorce case in Chicago</h3></span>
-							<span class="authors">
-								<p>By Clark Kent and Jimmy Olsen - 3 hours ago</p>
-							</span>
+						<span class="image">
+							<img src="https://suntimesmedia.files.wordpress.com/2017/01/sneedmccarthy012717.jpg?w=80">
+						</span>
+						<h3 class="title">Jesse Jackson Jr. dismisses divorce case in Chicago</h3>
+						<p class="authors">By Clark Kent and Jimmy Olsen - 3 hours ago</p>
 					</div>
 					<div class="columns small-12 medium-6" data-equalizer-watch>
-							<span class="image">
-								<img src="https://suntimesmedia.files.wordpress.com/2017/04/summers-042617-5.jpg?w=80">
-							</span>
-							<span class="title"><h3>Brown: Summers' floating of own candidacy could buoy Pritzker</h3></span>
-							<span class="authors">
-								<p>By Clark Kent and Jimmy Olsen - 4 hours ago</p>
-							</span>
+						<span class="image">
+							<img src="https://suntimesmedia.files.wordpress.com/2017/04/summers-042617-5.jpg?w=80">
+						</span>
+						<h3 class="title">Brown: Summers' floating of own candidacy could buoy Pritzker</h3>
+						<p class="authors">By Clark Kent and Jimmy Olsen - 4 hours ago</p>
 					</div>
 					<div class="columns small-12 medium-6" data-equalizer-watch>
-							<span class="image">
-								<img src="https://suntimesmedia.files.wordpress.com/2017/04/rondo13.jpg?w=80">
-							</span>
-							<span class="title"><h3>Rajon Rondo is still out for Game 5 — and shoots down report</h3></span>
-							<span class="authors">
-								<p>By Clark Kent and Jimmy Olsen - 1 day ago</p>
-							</span>
+						<span class="image">
+							<img src="https://suntimesmedia.files.wordpress.com/2017/04/rondo13.jpg?w=80">
+						</span>
+						<h3 class="title">Rajon Rondo is still out for Game 5 — and shoots down report</h3>
+						<p class="authors">By Clark Kent and Jimmy Olsen - 1 day ago</p>
 					</div>
 				</div>
 				<div class="other-stories">
@@ -103,4 +101,5 @@
 	<div class="columns small-11 medium-4 large-3 sidebar small-centered medium-uncentered">
 		<img src="http://placehold.it/300x600&amp;text=[ad]">
 	</div>
+	<hr>
 </div>
