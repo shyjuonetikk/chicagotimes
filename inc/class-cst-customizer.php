fafec5bd370739751427d0ca4a1628e5b445c43c
Sean Hayes <seanchayes@gmail.com>
2017-05-11 14:21:07 -0500

    Removing unneeded customizer settings, moving breaking news styles into update homepage scss

--- inc/class-cst-customizer.php
+++ inc/class-cst-customizer.php
@@ -22,7 +22,9 @@ class CST_Customizer {
 	private function setup_actions() {
 
 		add_action( 'customize_register', array( $this, 'action_customize_register' ), 11 );
-
+		if ( method_exists( 'Jetpack_Custom_CSS', 'disable' ) ) {
+			add_action( 'init', array( 'Jetpack_Custom_CSS', 'disable' ), 11 );
+		}
 	}
 
 	/**
@@ -34,25 +36,8 @@ class CST_Customizer {
 		// and we don't want the homepage to change
 		$customize->remove_section( 'title_tagline' );
 		$customize->remove_section( 'static_front_page' );
-
-		$customize->add_section( 'cst_site_header', array(
-			'title'          => esc_html__( 'Site Header', 'chicagosuntimes' ),
-			'priority'       => 35,
-		) );
-	 
-		$customize->add_setting( 'cst_headline_template', array(
-			'default'        => 'cst_images',
-		) );
-	 
-		$customize->add_control( 'cst_headline_template', array(
-			'label'   => esc_html__( 'Featured articles template:', 'chicagosuntimes' ),
-			'section' => 'cst_site_header',
-			'type'    => 'select',
-			'choices'    => array(
-				'cst_images' => esc_html__( 'Images', 'chicagosuntimes' ),
-				'cst_list' => esc_html__( 'List', 'chicagosuntimes' ),
-				),
-		) );
+		$customize->remove_section( 'colors' );
+		$customize->remove_section( 'custom_css' );
 
 	}
 
