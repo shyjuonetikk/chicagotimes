3e0b65ecc4ee55c4b641005fe8388a255d76bbd5
Sean Hayes <seanchayes@gmail.com>
2017-05-25 14:58:42 -0500

    Restored STNG wire widget, removed legacy widget function call in frontpage, removing some add conditionals, restoring sidebar calls

--- front-page.php
+++ front-page.php
@@ -40,7 +40,6 @@
 								<?php CST()->frontend->mini_stories_content_block( $other_stories, 'regular' ); ?>
 							</div>
 						</div><!-- hp-other-lead -->
-						<hr>
 						<div class="other-stories show-for-large-up">
 							<hr>
 							<h2>Trending in the Chicago Sun-Times (Chartbeat)</h2>
@@ -87,38 +86,18 @@
 				</div>
 			</div><!-- /.stories -->
 			<div class="columns small-12 medium-4 large-3 sidebar homepage-sidebar widgets">
-				<?php if ( get_query_var( 'showads', false ) ) { ?>
 					<?php if ( is_active_sidebar( 'homepage_sidebar' ) ) {
 						dynamic_sidebar( 'homepage_sidebar' );
 					} ?>
-				<?php } ?>
-				<?php if ( get_query_var( 'showads', false ) ) { ?>
 					<div class="cst-ad-container">
 						<hr>
 						<img src="http://placehold.it/300x250/e0e0e0/130100&amp;text=[300x250-ad-will-be-responsive]">
 					</div>
-				<?php } ?>
 				<div class="more-stories-container hide-for-xlarge-up">
 					<hr>
 					<?php if ( is_active_sidebar( 'homepage_sidebar_two' ) ) {
 						dynamic_sidebar( 'homepage_sidebar_two' );
 					} ?>
-				</div>
-				<div class="row more-stories-container hide-for-landscape">
-					<div class="small-12 columns">
-						<hr>
-						<h3 class="more-sub-head"><a href="<?php echo esc_url( '/' ); ?>">Entertainment</a></h3>
-						<?php
-						$query = array(
-							'post_type'           => array( 'cst_article' ),
-							'ignore_sticky_posts' => true,
-							'posts_per_page'      => 4,
-							'post_status'         => 'publish',
-							'cst_section'         => 'entertainment',
-							'orderby'             => 'modified',
-						);
-						CST()->frontend->cst_mini_stories_content_block( $query ); ?>
-					</div>
 				</div>
 				<div>
 					<hr>
@@ -127,13 +106,13 @@
 				<div class="show-for-medium-up">
 					<hr>
 					<?php if ( get_query_var( 'showads', false ) ) { ?>
-						<img src="http://placehold.it/300x250/a0d0a0/130100&amp;text=[300x250-ad-will-be-responsive]">
+						<img src="http://placehold.it/300x250/a0d0a0/130100&amp;text=[300x250]">
 					<?php } ?>
 				</div>
 				<div class="hide-for-medium-down">
 					<hr>
 					<div class="row">
-						<?php //the_widget( 'CST_STNG_Wire_Widget' ); ?>
+						<?php the_widget( 'CST_STNG_Wire_Widget' ); ?>
 					</div>
 				</div>
 			</div>
