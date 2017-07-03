b99d057381be214b50d83a5c68364cb60b7cf72c
Sean Hayes <seanchayes@gmail.com>
2017-07-03 13:58:36 -0500

    Better determination of section header display name and section anchor link

--- parts/homepage/column-wells.php
+++ parts/homepage/column-wells.php
@@ -11,11 +11,23 @@
 ?>
 <section id="section-column-wells">
     <div class="row" data-equalizer>
-<?php foreach ( $column_well_sections as $section_slug => $sponsored_key ) { ?>
+<?php foreach ( $column_well_sections as $section_slug => $sponsored_key ) {
+	$term = wpcom_vip_get_term_by( 'slug', $section_slug, 'cst_section' );
+	if ( $term ) {
+		$section_term_link = wpcom_vip_get_term_link( $section_slug, 'cst_section' );
+		if ( is_wp_error( $section_term_link ) ) {
+			$section_link = home_url( '/' ) . 'section/' . esc_attr( $section_slug ) . '/';
+			$section_name = $section_slug;
+		} else {
+			$section_link = $section_term_link;
+			$section_name = $term->name;
+		}
+	}
+	?>
 	<div class="large-3 medium-6 small-12 columns" data-equalizer-watch>
 		<div class="section-list">
-			<h3 class="<?php echo esc_attr( $section_slug ); ?>-border"><a href="<?php echo esc_url( home_url( '/' ) . 'section/' . esc_attr( $section_slug ) . '/' ); ?>" data-on="click" data-event-category="navigation"
-									   data-event-action="navigate-hp-<?php echo esc_attr( $section_slug ); ?>-column-title"><?php esc_html_e( ucfirst( $section_slug ), 'chicagosuntimes' ); ?></a></h3>
+			<h3 class="<?php echo esc_attr( $section_slug ); ?>-border"><a href="<?php echo esc_url( $section_link ); ?>" data-on="click" data-event-category="navigation"
+									   data-event-action="navigate-hp-<?php echo esc_attr( $section_slug ); ?>-column-title"><?php esc_html_e( ucfirst( $section_name ), 'chicagosuntimes' ); ?></a></h3>
 			<?php $query = array(
 				'post_type'           => array( 'cst_article' ),
 				'ignore_sticky_posts' => true,
