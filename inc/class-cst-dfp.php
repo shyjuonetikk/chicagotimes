4432b965f90fa2e84ce553235906b5d91f3fd10f
Sean Hayes <seanchayes@gmail.com>
2017-07-17 14:45:01 -0500

    Removing the restoration of 250 ad size as we would still only like up to a 90

--- inc/class-cst-dfp.php
+++ inc/class-cst-dfp.php
@@ -315,8 +315,8 @@ var dfp = {
     addSize([0, 0], [[320, 50], [300, 50]]). //other
     build();
     hp_upper_super_leaderboard_mapping = googletag.sizeMapping().
-    addSize([1200, 800], [ [970, 250], [970,90], [728,90] ] ). //tablet
-    addSize([992, 0], [ [970, 250], [970, 90], [728, 90] ] ). //desktop
+    addSize([1200, 800], [ [970,90], [728,90] ] ). //tablet
+    addSize([992, 0], [ [970, 90], [728, 90] ] ). //desktop
     addSize([800, 1200], [ [728,90] ] ). //tablet
     addSize([768, 1024], [ [728,90] ] ). //tablet
     addSize([640, 480], [[320, 50], [300, 50]]). //phone
