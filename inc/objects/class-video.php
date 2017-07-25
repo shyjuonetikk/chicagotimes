9d323b4d17b31611d4f39aa920cd3b005372f3b8
Sean Hayes <seanchayes@gmail.com>
2017-07-25 11:47:23 -0500

    Creating embed handler part, updating kses to allow Cube video embed, updating video post type to allow Cube embeds

--- inc/objects/class-video.php
+++ inc/objects/class-video.php
@@ -30,15 +30,22 @@ class Video extends Post {
 	public function get_video_embed() {
 
 		$video_url = $this->get_video_url();
-		$host = parse_url( $video_url, PHP_URL_HOST );
-
+		$host = wp_parse_url( $video_url, PHP_URL_HOST );
+		$render_host = '';
 		// ex http://youtu.be/HJMapA8WgYw
-		if ( in_array( $host, array( 'youtu.be' ) ) ) {
-			$video_id = trim( parse_url( $video_url, PHP_URL_PATH ), '/' );
+		if ( in_array( $host, [ 'youtu.be' ], true ) ) {
+			$video_id = trim( wp_parse_url( $video_url, PHP_URL_PATH ), '/' );
+			$render_host = 'youtube';
 		// ex https://www.youtube.com/watch?v=HJMapA8WgYw&feature=youtu.be
-		} else if ( in_array( $host, array( 'youtube.com', 'www.youtube.com' ) ) ) {
-			parse_str( parse_url( $this->get_video_url(), PHP_URL_QUERY ), $youtube_vars );
+		} else if ( in_array( $host, [ 'youtube.com', 'www.youtube.com' ], true ) ) {
+			parse_str( wp_parse_url( $this->get_video_url(), PHP_URL_QUERY ), $youtube_vars );
 			$video_id = ( ! empty( $youtube_vars['v'] ) ) ? $youtube_vars['v'] : '';
+			$render_host = 'youtube';
+		} else if ( in_array( $host, [ 'thecube.com' ], true ) ) {
+			if ( preg_match( '/([\d]{3,7})$/', $this->get_video_url(), $matches ) ) {
+				$video_id = $matches[0];
+				$render_host = 'cube';
+			}
 		} else {
 			$video_id = '';
 		}
@@ -47,7 +54,17 @@ class Video extends Post {
 			return '';
 		}
 
-		$iframe = '<iframe class="cst-responsive" data-true-height="640" data-true-width="360" width="100%" height="320px" src="' . esc_url( '//www.youtube.com/embed/' . sanitize_text_field( $video_id ) ) . '" frameborder="0"></iframe>';
+		switch ( $render_host ) {
+			case 'youtube':
+				$iframe = '<iframe class="cst-responsive" data-true-height="640" data-true-width="360" width="100%" height="320px" src="' . esc_url( '//www.youtube.com/embed/' . sanitize_text_field( $video_id ) ) . '" frameborder="0"></iframe>';
+				break;
+			case 'cube':
+				$iframe = '<iframe src="//thecube.com/embed/' . esc_attr( $video_id )  . '" width="640" height="460" frameborder="0" scrolling="no" allowtransparency="true" allowfullscreen mozallowfullscreen webkitallowfullscreen></iframe><div><a style="font-size:11px" href="http://thecube.com">Share Events on The Cube</a></div>';
+				break;
+			default:
+				$iframe = '';
+		}
+
 		return $iframe;
 
 	}
---
+++ parts/embed/video.php
@@ -0,0 +1,1 @@
+<?php echo wp_kses( $obj->get_video_embed(), CST()->video_iframe_kses );
