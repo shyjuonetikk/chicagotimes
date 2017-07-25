c560d85afe8d352ea0f40498b47ec4563c910c66
Sean Hayes <seanchayes@gmail.com>
2017-07-25 11:47:23 -0500

    Creating embed handler part, updating kses to allow Cube video embed, updating video post type to allow Cube embeds

--- functions.php
+++ functions.php
@@ -106,13 +106,27 @@ class CST {
 			'data-event-category' => array(),
 		),
 	);
-	public $sendtonews_kses = array(
+	public $video_iframe_kses = array(
 		'iframe' => array(
-			'id' => array(), 'class' => array(), 'src' => array(), 'style' => array(), 'allowfullscreen' => array(), 'frameborder' => array(), 'scrolling' => array(), 'marginheight' => array(), 'marginwidth' => array(), 'width' => array(), 'height' => array()
+			'id' => array(),
+			'class' => array(),
+			'src' => array(),
+			'style' => array(),
+			'allowfullscreen' => array(),
+			'frameborder' => array(),
+			'scrolling' => array(),
+			'marginheight' => array(),
+			'marginwidth' => array(),
+			'width' => array(),
+			'height' => array(),
+			'allowtransparency' => array(),
+			'mozallowfullscreen' => [],
+			'webkitallowfullscreen' => [],
 		),
 		'span' => array( 'class' => array() ),
 		'div' => array( 'class' => array(), 'data-type' => array() ),
 		'p' => array(),
+		'a' => array(),
 		'script' => array(
 			'class' => array(),
 			'type' => array(),
