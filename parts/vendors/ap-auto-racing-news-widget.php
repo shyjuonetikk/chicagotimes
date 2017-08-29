514215cffbfc5422ac8f0b24e6660070cb785934
Sean Hayes <seanchayes@gmail.com>
2017-08-29 13:23:41 -0500

    Updating AP widgets to https

--- parts/vendors/ap-auto-racing-news-widget.php
+++ parts/vendors/ap-auto-racing-news-widget.php
@@ -1,2 +1,2 @@
 <div id="ap_widget_container_ea385cf84e0c7c1042d5d0677a97c185"></div>
-<script src="http://digitalservices.ap.org/widget-content/ea385cf84e0c7c1042d5d0677a97c185"></script>
+<script src="https://digitalservices.ap.org/widget-content/ea385cf84e0c7c1042d5d0677a97c185"></script>
