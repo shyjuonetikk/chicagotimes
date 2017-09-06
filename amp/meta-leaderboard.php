6b933c998c0afafa665077430e13bb06f923a8d7
Sean Hayes <seanchayes@gmail.com>
2017-09-06 14:12:09 -0500

    Removing AMP leaderboard code for time being in production

--- amp/meta-leaderboard.php
+++
@@ -1,3 +0,0 @@
-<div class="ad-center">
-	<amp-ad width="320" height="50" type="doubleclick" data-slot ="<?php echo esc_attr( CST()->dfp_handler->ad_header_settings( true ) ); ?>" json='{"targeting":{"pos":"AMP Ad"}}'></amp-ad>
-</div>
