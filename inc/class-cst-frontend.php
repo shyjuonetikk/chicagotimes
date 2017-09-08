c3984b06b091d4f05c7c08a62400652012cd81cb
Sean Hayes <seanchayes@gmail.com>
2017-09-08 11:53:43 -0500

    Addressing https on dev for chartbeat stats

--- inc/class-cst-frontend.php
+++ inc/class-cst-frontend.php
@@ -2176,16 +2176,13 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	* Display Chartbeat engagement based article list on home page
 	*/
 	public function enqueue_chartbeat_react_engagement_script() {
-		if ( function_exists('jetpack_is_mobile') && ! jetpack_is_mobile() ) {
+		if ( is_front_page() && function_exists('jetpack_is_mobile') && ! jetpack_is_mobile() ) {
 			$site = CST()->dfp_handler->get_parent_dfp_inventory();
+			$chartbeat_file_name = 'main.3f878c34-cb-dev-test.js';
 			if ( 'chicago.suntimes.com' === $site ) {
 				$chartbeat_file_name = 'main.54a95b28-cb-prod.js';
-			} else {
-				$chartbeat_file_name = 'main.81b31ab6-cb-dev-test.js';
 			}
-			if ( is_front_page() ) {
-				wp_enqueue_script( 'chartbeat_engagement', esc_url( get_stylesheet_directory_uri() . '/assets/js/' . $chartbeat_file_name ), array(), null, true );
-			}
+			wp_enqueue_script( 'chartbeat_engagement', esc_url( get_stylesheet_directory_uri() . '/assets/js/' . $chartbeat_file_name ), [], null, true );
 		}
 	}
 	/**
