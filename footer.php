08a2c833a2894ca71b4a42374a89d711a0fd6bdd
shyjuonetikk <31687875+shyjuonetikk@users.noreply.github.com>
2017-09-26 15:00:18 +0530

    Add files via upload
    Included the path of taboola script on both header and footer files.
    Created function for getting the location key for getting the Radar image details.
     Static value "chicago" is passing through the function for now, as on current scope.

--- footer.php
+++ footer.php
@@ -3,15 +3,17 @@
 <a class="exit-off-canvas"></a>
 </main>
 </div><!-- /off-canvas-wrap -->
-
+<script type="text/javascript" src="<?php echo esc_url( get_template_directory_uri());?>/assets/js/vendor/taboola-header-weather.js" ></script>
 <?php
 if ( is_singular() ) {
 	get_template_part( 'parts/vendors/yieldmo-footer' );
+	get_template_part( 'parts/vendors/aggrego-headlinesnetwork-footer' );
 }
 get_template_part( 'parts/analytics/chartbeat-footer' );
 get_template_part( 'parts/footer-brand-navigation' );
 
 do_action( 'closing_body' );
+
 ?>
 <?php wp_footer(); ?>
 </body>
--- page-weather.php
+++ page-weather.php
@@ -21,10 +21,37 @@
 						<source src="https://dig.abclocal.go.com/wls/video/SunTimes/wls_vid_wx_planner.ogv?vs=20141101 21:16:01" type='video/ogg' />
 					</video>
 				</div>
+				<div class="weather-radar-container">
+					<h2><?php echo esc_html_e( 'Weather Radar', 'chicagosuntimes' ); ?></h2>
+					<div class="image-container">
+						<img src="<?php echo getWeatherImage('chicago'); ?>">
+					</div>
+					<div class="row">
+						<div class="columns">
+							<hr>
+						</div>
+						<div id="circularhub_module_10897"></div>
+						<script src="//api.circularhub.com/10897/2e2e1d92cebdcba9/circularhub_module.js"></script>
+						<?php
+							$obj = \CST\Objects\Post::get_by_post_id( get_the_ID() );
+							$classes = array( 'single-view', 'columns', 'small-12', 'column-adjust', 'end', 'cst-sharing-relative' );
+							$attrs = CST()->frontend->article_dimensions( $obj );
+						?>
+					</div>
+					<article <?php post_class( $classes ); ?> <?php echo wp_kses_post( $attrs ); ?>>
+								<?php 
+									echo wp_kses( CST()->get_template_part( 'post/post-recommendations-chartbeat', array( 'obj' => $obj ) ), CST()->recommendation_kses ); 
+								?>
+
+					</article>
+					<?php get_template_part( 'parts/taboola/taboola-weather' ); ?>
+				</div>
 			</div>
 			<div class="large-4 columns weather-sidebar">
 				<script src="https://content.synapsys.us/embeds/placement.js?p=VSUE4YV38U&#038;type=dynamic_group_weather&#038;style=standard"></script>
+				<?php echo wp_kses( CST()->dfp_handler->unit( 1, 'div-gpt-sky-scraper', 'dfp' ), CST()->dfp_kses ); ?>
 			</div>
+
 		</div>
 	</section>
 
