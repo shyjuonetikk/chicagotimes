98bcec0c933bca1031ff0f2d312b8338cf1b51a1
Sean Hayes <seanchayes@gmail.com>
2017-10-13 16:33:00 -0500

    Sortable section headings, tracked via hidden field, needs review to check rendering

--- assets/js/cst-customize-control-sorter.js
+++ assets/js/cst-customize-control-sorter.js
@@ -4,9 +4,21 @@
   api.controlConstructor.cst_sf_sorter_control = api.Control.extend({
     ready: function() {
       var control = this;
+      var list;
       var element = $("#" + control.id);
+      var collection = $("#" + control.id + '-collection');
       api.Control.prototype.ready.call(control);
-      element.sortable();
+      element.sortable({
+        change: function (event, ui) {
+          console.log( ui.item[0].innerText + ' changed' )
+          list = element.find('.cst-item')
+          var inputValues = list.map(function() {
+            return $(this).data('key');
+          }).toArray();
+          collection.val(inputValues);
+          collection.trigger('change')
+        }
+      });
       console.log( 'Sorter ready ' + element );
     }
   });
