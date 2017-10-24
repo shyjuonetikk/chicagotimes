1a6fcd8e6835bf3301e22fdfdc84a226fcde6dc8
Sean Hayes <seanchayes@gmail.com>
2017-10-24 14:34:16 -0500

    Adding defer to AP widgets to see if it helps pageloads when in customizer (and beyond)

--- parts/vendors/ap-auto-racing-nascar-standings-widget.php
+++ parts/vendors/ap-auto-racing-nascar-standings-widget.php
@@ -1,2 +1,2 @@
 <div id="ap_widget_container_77e89a1b312a1a4beeccf8f8d7e98811"></div>
-<script src="https://digitalservices.ap.org/widget-content/77e89a1b312a1a4beeccf8f8d7e98811"></script>
+<script defer src="https://digitalservices.ap.org/widget-content/77e89a1b312a1a4beeccf8f8d7e98811"></script>
