5b909a60428d9affd5c0c7cbda234900b91fee75
Sean Hayes <seanchayes@gmail.com>
2017-10-25 15:57:28 -0500

    Whitespacing

--- inc/cst-customizer-controls.php
+++ inc/cst-customizer-controls.php
@@ -3,7 +3,6 @@ if ( class_exists( 'WP_Customize_Control' ) ) {
 	/**
 	 * CST Custom controllers
 	 */
-
 	require_once dirname( __FILE__ ) . '/controls/class-cst-select-control.php';
 	require_once dirname( __FILE__ ) . '/controls/class-cst-sf-sorter-control.php';
 }
