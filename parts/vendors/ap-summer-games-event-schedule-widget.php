1899da76704fc6d925a3e95701b771eeb8f2153f
Sean Hayes <seanchayes@gmail.com>
2017-10-25 12:19:39 -0500

    Reverting defer addition

--- parts/vendors/ap-summer-games-event-schedule-widget.php
+++ parts/vendors/ap-summer-games-event-schedule-widget.php
@@ -1,2 +1,2 @@
 <div id="ap_widget_container_f3e70ec4be83786a7fe13c9bc49aaea0"></div>
-<script defer src="https://dne.ap.org/widget-content/f3e70ec4be83786a7fe13c9bc49aaea0"></script>
+<script src="https://dne.ap.org/widget-content/f3e70ec4be83786a7fe13c9bc49aaea0"></script>
--- parts/vendors/ap-summer-games-medal-count-widget.php
+++ parts/vendors/ap-summer-games-medal-count-widget.php
@@ -1,2 +1,2 @@
 <div id="ap_widget_container_298ad90de2501b670491db5634f3db69"></div>
-<script defer src="https://dne.ap.org/widget-content/298ad90de2501b670491db5634f3db69"></script>
+<script src="https://dne.ap.org/widget-content/298ad90de2501b670491db5634f3db69"></script>
