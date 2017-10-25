c13e23f1de39d4036760497ab4f60e74bd81ae50
Sean Hayes <seanchayes@gmail.com>
2017-10-25 13:23:24 -0500

    Adding header adjustments to prevent unintentional margin

--- parts/homepage/header.php
+++ parts/homepage/header.php
@@ -1,16 +1,15 @@
 <?php
 get_template_part( 'parts/header-brand-navigation' );
 ?>
-<div class="homepage_wrapper">
-	<div class="hp-bn-wrapper">
-		<ul>
-		<?php
-		if ( is_active_sidebar( 'homepage_breaking_news' ) ) :
-			dynamic_sidebar( 'homepage_breaking_news' );
-		endif;
-		?>
+	<div class="homepage_wrapper">
+<?php
+if ( is_active_sidebar( 'homepage_breaking_news' ) ) { ?>
+	<div class="hp-bn-wrapper" >
+		<ul >
+			<?php dynamic_sidebar( 'homepage_breaking_news' ); ?>
 		</ul>
 	</div>
+<?php } ?>
 <?php
 if ( ( is_home() || is_front_page() ) ) {
 	echo wp_kses( CST()->dfp_handler->dynamic_unit( 2, 'div-gpt-super-leaderboard', 'dfp dfp-super-leaderboard dfp-centered', 'hp_upper_super_leaderboard_mapping', 'Super leaderboard 2 970x90' ),
