a3f71365a01bc7a4daf6eceb20d884966566544c
RichM <rmcdonald@suntimes.com>
2017-11-16 11:01:25 -0600

    resolve conflict

--- inc/widgets/class-cst-ad-widget.php
+++ inc/widgets/class-cst-ad-widget.php
@@ -12,7 +12,6 @@ class CST_Ad_Widget extends WP_Widget {
 		'dfp-rr-cube-6' => 'rr cube 6',
 		'dfp-rr-cube-promo-7' => 'rr cube 7',
 		'dfp-polar-8' => 'rr cube 8',
-		'dfp-pro300x600' => 'pro 300x600',
 		);
 
 	protected $defaults = array(
