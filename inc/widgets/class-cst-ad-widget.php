29540445f0c075e057d559dbc9bd328a7e55d23a
RichM <rmcdonald@suntimes.com>
2017-11-16 11:00:30 -0600

    resolve conflict

--- inc/widgets/class-cst-ad-widget.php
+++ inc/widgets/class-cst-ad-widget.php
@@ -3,31 +3,32 @@
 class CST_Ad_Widget extends WP_Widget {
 
 
-	protected $ad_units = [
-		'dfp-rr-cube-1'       => 'rr cube 1',
-		'dfp-rr-cube-2'       => 'rr cube 2',
-		'dfp-rr-cube-3'       => 'rr cube 3',
-		'dfp-rr-cube-4'       => 'rr cube 4',
-		'dfp-rr-cube-5'       => 'rr cube 5',
-		'dfp-rr-cube-6'       => 'rr cube 6',
+	protected $ad_units = array(
+		'dfp-rr-cube-1' => 'rr cube 1',
+		'dfp-rr-cube-2' => 'rr cube 2',
+		'dfp-rr-cube-3' => 'rr cube 3',
+		'dfp-rr-cube-4' => 'rr cube 4',
+		'dfp-rr-cube-5' => 'rr cube 5',
+		'dfp-rr-cube-6' => 'rr cube 6',
 		'dfp-rr-cube-promo-7' => 'rr cube 7',
-		'dfp-polar-8'         => 'rr cube 8',
-	];
+		'dfp-polar-8' => 'rr cube 8',
+		'dfp-pro300x600' => 'pro 300x600',
+		);
 
-	protected $defaults = [
-		'ad_unit' => 'dfp-rr-cube-1',
-	];
+	protected $defaults = array(
+		'ad_unit'  => 'dfp-rr-cube-1',
+		);
 
 	public function __construct() {
-		$widget_ops = [
-			'description'                 => esc_html__( 'Displays a right rail ad unit.', 'chicagosuntimes' ),
-			'customize_selective_refresh' => true,
-		];
+
 		parent::__construct(
 			'cst_ad_widget',
 			esc_html__( 'CST Right Rail Ad Widget', 'chicagosuntimes' ),
-			$widget_ops
+			array(
+				'description' => esc_html__( 'Displays a right rail ad unit.', 'chicagosuntimes' ),
+			)
 		);
+
 	}
 
 	public function widget( $args, $instance ) {
@@ -35,20 +36,17 @@ class CST_Ad_Widget extends WP_Widget {
 		$instance = array_merge( $this->defaults, $instance );
 
 		$targeting_name = $this->ad_units[ $instance['ad_unit'] ];
-		$widget_number  = explode( '-', $this->id );
-		$ad_unit_index  = (int) $widget_number[1] + 100;
+		$widget_number = explode( '-', $this->id );
+		$ad_unit_index = (int) $widget_number[1] + 100;
 		echo wp_kses_post( $args['before_widget'] );
 		if ( is_front_page() ) {
 			$mapping = 'hp_cube_mapping';
-		} elseif ( is_singular() ) {
+		} else if ( is_singular() ) {
 			$mapping = 'article_cube_mapping';
 		} else {
 			$mapping = 'sf_mapping';
 		}
-		if ( isset( $args['mapping'] ) ) {
-			$mapping = $args['mapping'];
-		}
-		echo wp_kses( CST()->dfp_handler->dynamic_unit( $ad_unit_index, 'div-gpt-rr-cube', 'dfp dfp-cube', $mapping, $targeting_name ),
+		echo wp_kses( CST()->dfp_handler->dynamic_unit( $ad_unit_index , 'div-gpt-rr-cube', 'dfp dfp-cube', $mapping, $targeting_name ),
 			CST()->dfp_kses
 		);
 		echo wp_kses_post( $args['after_widget'] );
@@ -65,21 +63,20 @@ class CST_Ad_Widget extends WP_Widget {
 
 		?>
 
-		<p>
-			<label for="<?php echo esc_attr( $this->get_field_id( 'ad_unit' ) ); ?>"><?php esc_html_e( 'Ad Unit', 'chicagosuntimes' ); ?>:</label>
-			<select class="widefat cst-ad-widget" id="<?php echo esc_attr( $this->get_field_id( 'ad_unit' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'ad_unit' ) ); ?>"
-					data-current-unit="<?php echo esc_attr( $current_unit ); ?>">
-				<?php foreach ( $this->ad_units as $ad_unit => $ad_target ) : ?>
-					<option value="<?php echo esc_attr( $ad_unit ); ?>" <?php selected( $ad_unit, $current_unit ); ?>><?php echo esc_html( $ad_unit ); ?></option>
-				<?php endforeach; ?>
-			</select>
-		</p>
+	<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'ad_unit' ) ); ?>"><?php esc_html_e( 'Ad Unit', 'chicagosuntimes' ); ?>:</label>
+		<select class="widefat cst-ad-widget" id="<?php echo esc_attr( $this->get_field_id( 'ad_unit' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'ad_unit' ) ); ?>" data-current-unit="<?php echo esc_attr( $current_unit ); ?>">
+		<?php foreach ( $this->ad_units as $ad_unit => $ad_target ) : ?>
+			<option value="<?php echo esc_attr( $ad_unit ); ?>" <?php selected( $ad_unit, $current_unit ); ?>><?php echo esc_html( $ad_unit ); ?></option>
+		<?php endforeach; ?>
+		</select>
+	</p>
 
 	<?php }
 
 	public function update( $new_instance, $old_instance ) {
 
-		$instance = [];
+		$instance = array();
 
 		if ( isset( $this->ad_units[ $new_instance['ad_unit'] ] ) ) {
 			$instance['ad_unit'] = $new_instance['ad_unit'];
