6b9746c90b88bedaed9196697aae7974558d57b3
RichM <rmcdonald@suntimes.com>
2017-12-06 11:18:04 -0600

    update ads.txt file based on master

--- page-rewrite-me.php
+++ page-rewrite-me.php
@@ -14,6 +14,11 @@ if ( $where_to && '' !== $where_to ) {
 			echo 'google-site-verification: googleddb5b02478e7d794.html';
 			die( 200 );
 			break;
+		case 'ads.txt':
+			header( 'Content-Type: text/plain; charset=' . get_option( 'blog_charset' ) );
+			echo wp_kses_post( wpcom_vip_file_get_contents( get_stylesheet_directory_uri() . '/ads.txt' ) );
+			die( 200 );
+			break;
 		default:
 			break;
 	}
