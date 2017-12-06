e13d2ef62957222590b0c649fdb64f680cde2330
Sean Hayes <seanchayes@gmail.com>
2017-12-06 09:39:45 -0600

    Making the title field accommodate 80 characters

--- inc/objects/class-ap-wire-item.php
+++ inc/objects/class-ap-wire-item.php
@@ -75,7 +75,8 @@ class AP_Wire_Item extends Post {
 			$post->set_wire_headline( sanitize_text_field( (string) $feed_entry->title ) );
 		}
 		if(!empty($feed_entry->blocks)) {
-			$post->set_wire_content( wp_filter_post_kses( $feed_entry->blocks));
+//			$post->set_wire_content( wp_filter_post_kses( $feed_entry->blocks));
+
 		}
 
 		return $post;
